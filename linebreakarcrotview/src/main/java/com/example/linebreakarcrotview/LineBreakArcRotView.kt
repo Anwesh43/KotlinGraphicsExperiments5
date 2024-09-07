68b0695415c90b8333168c6f396d75dc60eb0533
Anwesh43 <anweshthecool0@gmail.com>
2024-09-07 14:10:03 +0530

    linebreakarcrotview: adding LineBreakArcRot class

--- linebreakarcrotview/src/main/java/com/example/linebreakarcrotview/LineBreakArcRotView.kt
+++ linebreakarcrotview/src/main/java/com/example/linebreakarcrotview/LineBreakArcRotView.kt
@@ -165,4 +165,27 @@ class LineBreakArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBreakArcRot(var i : Int) {
+
+        private var curr : LBARNode = LBARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
