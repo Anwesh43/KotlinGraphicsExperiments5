c3e31abd45acb33cec8238d07703d01c83cbd620
Anwesh43 <anweshthecool0@gmail.com>
2024-09-07 15:00:37 +0530

    linebreakarcrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.lineextendarcrotview.LineExtendArcRotView
+import com.example.linebreakarcrotview.LineBreakArcRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineExtendArcRotView.create(this)
+        LineBreakArcRotView.create(this)
     }
 }
