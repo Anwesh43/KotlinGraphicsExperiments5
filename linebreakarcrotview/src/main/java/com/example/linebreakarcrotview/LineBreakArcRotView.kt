c3c8cc66025ae489a033dfd625ace103565d7b54
Anwesh43 <anweshthecool0@gmail.com>
2024-09-07 12:42:59 +0530

    linebreakarcrotview: adding LineBreakArcRotView class

--- linebreakarcrotview/src/main/java/com/example/linebreakarcrotview/LineBreakArcRotView.kt
+++ linebreakarcrotview/src/main/java/com/example/linebreakarcrotview/LineBreakArcRotView.kt
@@ -62,3 +62,18 @@ fun Canvas.drawLBARNode(i : Int, scale : Float, paint : Paint) {
     drawLineBreakArcRot(scale, w, h, paint)
 }
 
+class LineBreakArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
