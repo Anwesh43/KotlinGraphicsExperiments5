6d685f0c697cd14827d4c2b286c7265a1b3392c8
Anwesh43 <anweshthecool0@gmail.com>
2024-09-09 08:25:38 +0530

    arctouprotview: adding ArcToUpRotView class

--- arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
+++ arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawATURNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcToUpRot(scale, w, h, paint)
 }
+
+class ArcToUpRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
