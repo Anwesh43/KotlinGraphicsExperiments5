b286019a417c1506ebdd4c14d24b5291c2d975ef
Anwesh43 <anweshthecool0@gmail.com>
2024-09-09 08:24:10 +0530

    arctouprotview: adding Canvas extension methods

--- arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
+++ arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,35 @@ val rot : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcToUpRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + paint.strokeWidth) * dsc(4), h / 2) {
+        drawXY(0f, (h / 2 - size / 2) * (1 - dsc(2))) {
+            rotate(rot * dsc(3))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * dsc(0), false, paint)
+        }
+        drawXY(0f, -(h / 2) * (1 - dsc(1))) {
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawATURNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcToUpRot(scale, w, h, paint)
+}
