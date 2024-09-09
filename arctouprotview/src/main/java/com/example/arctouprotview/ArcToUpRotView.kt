2b058384edbd7bd2a87a425c1177c091e4684c86
Anwesh43 <anweshthecool0@gmail.com>
2024-09-09 20:49:51 +0530

    arctouprotview: adding Renderer in View

--- arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
+++ arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
@@ -62,14 +62,15 @@ fun Canvas.drawATURNode(i : Int, scale : Float, paint : Paint) {
 
 class ArcToUpRotView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
