f94273d9e5b38e3ade7f7ab28c66659bd073ff88
Anwesh43 <anweshthecool0@gmail.com>
2024-09-09 20:53:10 +0530

    arctouprotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcintolinedownview.ArcIntoLineDownView
+import com.example.arctouprotview.ArcToUpRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcIntoLineDownView.create(this)
+        ArcToUpRotView.create(this)
     }
 }
