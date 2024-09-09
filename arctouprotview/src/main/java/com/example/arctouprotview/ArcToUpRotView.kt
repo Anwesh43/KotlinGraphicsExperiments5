eb5903b8a39ed91436dfdf738551d50f3de660c3
Anwesh43 <anweshthecool0@gmail.com>
2024-09-09 20:54:21 +0530

    arctouprotview: adding fix

--- arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
+++ arctouprotview/src/main/java/com/example/arctouprotview/ArcToUpRotView.kt
@@ -57,6 +57,7 @@ fun Canvas.drawATURNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawArcToUpRot(scale, w, h, paint)
 }
 
