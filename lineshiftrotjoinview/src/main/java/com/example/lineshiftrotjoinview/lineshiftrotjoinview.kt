33449b731d2ec2856920ca337dceb4af70ac8bd0
Anwesh43 <anweshthecool0@gmail.com>
2024-09-12 08:19:56 +0530

    lineshiftrotjoinview: adding LineShiftRotJoinView class

--- lineshiftrotjoinview/src/main/java/com/example/lineshiftrotjoinview/lineshiftrotjoinview.kt
+++ lineshiftrotjoinview/src/main/java/com/example/lineshiftrotjoinview/lineshiftrotjoinview.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLSRJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineShiftJoin(scale, w, h, paint)
 }
+
+class LineShiftRotJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
