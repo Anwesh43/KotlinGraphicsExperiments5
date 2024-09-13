c00419db7ca1643c625aef057bb3fb369bbf6193
Anwesh43 <anweshthecool0@gmail.com>
2024-09-13 11:01:27 +0530

    linemainbibentview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.lineshiftrotjoinview.LineShiftRotJoinView
+import com.example.linemainbibentview.LineMainBiBentView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineShiftRotJoinView.create(this)
+        LineMainBiBentView.create(this)
     }
 }
