0c22032f2b3a45ab726ef0d8383e12cbd9e7eaf4
Anwesh43 <anweshthecool0@gmail.com>
2024-09-14 09:20:11 +0530

    triarcjoinrotview: adding TAJRNode class

--- triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
+++ triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
@@ -125,4 +125,45 @@ class TriArcJoinRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TAJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TAJRNode? = null
+        private var prev : TAJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TAJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTAJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TAJRNode {
+            var curr : TAJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
