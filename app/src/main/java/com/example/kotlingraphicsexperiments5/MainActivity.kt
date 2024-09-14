e46d3e67abccc8a25d565e93d172e5ae1b84f08c
Anwesh43 <anweshthecool0@gmail.com>
2024-09-14 09:29:24 +0530

    triarcjoinrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linemainbibentview.LineMainBiBentView
+import com.example.triarcjoinrotview.TriArcJoinRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineMainBiBentView.create(this)
+        TriArcJoinRotView.create(this)
     }
 }
