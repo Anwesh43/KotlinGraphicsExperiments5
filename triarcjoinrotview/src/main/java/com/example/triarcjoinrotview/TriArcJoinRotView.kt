4f338292f1ac5c0aa958ebfa5bda7b98b4508108
Anwesh43 <anweshthecool0@gmail.com>
2024-09-14 09:11:58 +0530

    triarcjoinrotview: adding TriArcJoinRotView class

--- triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
+++ triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawTAJRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawTriArcJoinRot(scale, w, h, paint)
 }
+
+class TriArcJoinRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
