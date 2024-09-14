97ebfae94d9bf9081c0d4e9de0f65f14ecdf0626
Anwesh43 <anweshthecool0@gmail.com>
2024-09-14 09:10:05 +0530

    triarcjoinrotview: adding Canvas extension methods

--- triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
+++ triarcjoinrotview/src/main/java/com/example/triarcjoinrotview/TriArcJoinRotView.kt
@@ -19,11 +19,46 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val delay : Long = 20
-val sizeFactor : Float = 4.9f
+val sizeFactor : Float = 6.7f
 val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val n : Int = 3
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriArcJoinRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h * 0.5f * (1 + dsc(3))) {
+        rotate(rot * dsc(2))
+        for (k in 0..(n - 1)) {
+            drawXY(k * size, 0f) {
+                val dsc1 : Float = dsc(0).divideScale(k, n)
+                val dsc2 : Float = dsc(1).divideScale(n - 1 - k, n)
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * (dsc1 + dsc2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTAJRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawTriArcJoinRot(scale, w, h, paint)
+}
