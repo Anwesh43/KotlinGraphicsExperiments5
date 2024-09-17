c0d2738d5a937061741902b89badbe52bf8220ca
Anwesh43 <anweshthecool0@gmail.com>
2024-09-17 07:45:05 +0530

    singlerightlinebarview: adding SingleRightLineBarView class

--- singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
+++ singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawSRLBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSingleRightLineBar(scale, w, h, paint)
 }
+
+class SingleRightLineBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
