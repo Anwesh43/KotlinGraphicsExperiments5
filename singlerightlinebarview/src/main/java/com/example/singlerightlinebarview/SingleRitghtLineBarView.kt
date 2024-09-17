38cefe6d822d1799bf99c99c2983261b2a074b90
Anwesh43 <anweshthecool0@gmail.com>
2024-09-17 07:43:34 +0530

    singlerightlinebarview: adding Canvas extension method

--- singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
+++ singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
@@ -16,14 +16,50 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val sizeFactor : Float = 90f
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSingleRightLineBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        drawXY(0f, 0f) {
+            rotate(deg * dsc(3))
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(1))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+            drawXY(0f, -h * 0.5f * (1 - dsc(2))) {
+                drawRect(RectF(-size, -size, 0f, 0f), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSRLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSingleRightLineBar(scale, w, h, paint)
+}
