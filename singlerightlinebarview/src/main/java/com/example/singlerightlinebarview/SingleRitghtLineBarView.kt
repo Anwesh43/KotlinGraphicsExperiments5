98addb42ed3f17e1d3d24314b2eebedcbcaa5bd1
Anwesh43 <anweshthecool0@gmail.com>
2024-09-17 07:59:13 +0530

    singlerightlinebarview: adding Renderer class

--- singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
+++ singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
@@ -190,4 +190,27 @@ class SingleRightLineBarView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SingleRightLineBarView) {
+
+        private var srlb : SingleRightLineBar = SingleRightLineBar(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            srlb.draw(canvas, paint)
+            animator.animate {
+                srlb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            srlb.startupdating {
+                animator.start()
+            }
+        }
+    }
 }
