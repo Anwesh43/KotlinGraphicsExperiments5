83b6c92f817757b2dd51b71c2d5876bbe3b05e1a
Anwesh43 <anweshthecool0@gmail.com>
2024-09-17 07:36:53 +0530

    singlerightlinebarview: adding Int and Float extension methods

--- singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
+++ singlerightlinebarview/src/main/java/com/example/singlerightlinebarview/SingleRitghtLineBarView.kt
@@ -24,3 +24,6 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val sizeFactor : Float = 90f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
