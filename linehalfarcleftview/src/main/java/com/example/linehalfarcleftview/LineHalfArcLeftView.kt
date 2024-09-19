3bc7de31ef8a46578749a5e0db696ec332ab8355
Anwesh43 <anweshthecool0@gmail.com>
2024-09-19 12:14:12 +0530

    linehalfarcleftview: adding LHALNode class

--- linehalfarcleftview/src/main/java/com/example/linehalfarcleftview/LineHalfArcLeftView.kt
+++ linehalfarcleftview/src/main/java/com/example/linehalfarcleftview/LineHalfArcLeftView.kt
@@ -123,4 +123,45 @@ class LineHalfArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LHALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LHALNode? = null
+        private var prev : LHALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LHALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLHALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LHALNode {
+            var curr : LHALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
