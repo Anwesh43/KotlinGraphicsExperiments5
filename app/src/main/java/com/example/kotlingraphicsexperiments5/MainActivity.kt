8861688c379800fe396ffcf7c6a99fc39d82222c
Anwesh43 <anweshthecool0@gmail.com>
2024-09-19 12:20:33 +0530

    linehalfarcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.lineuparcrotview.LineUpArcRotView
+import com.example.linehalfarcleftview.LineHalfArcLeftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineUpArcRotView.create(this)
+        LineHalfArcLeftView.create(this)
     }
 }
