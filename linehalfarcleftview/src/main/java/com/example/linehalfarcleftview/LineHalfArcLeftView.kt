77b7c450a229597cca3caa156fbe3aa9e651ac2d
Anwesh43 <anweshthecool0@gmail.com>
2024-09-19 12:04:20 +0530

    linehalfarcleftview: adding LineHalfArcLeftView class

--- linehalfarcleftview/src/main/java/com/example/linehalfarcleftview/LineHalfArcLeftView.kt
+++ linehalfarcleftview/src/main/java/com/example/linehalfarcleftview/LineHalfArcLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLHALNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineHalfArcLeft(scale, w, h, paint)
 }
+
+class LineHalfArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
