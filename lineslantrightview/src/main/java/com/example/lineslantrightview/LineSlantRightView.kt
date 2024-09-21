aae1ea2b891c5418fd9db0e09d73209c624f59c1
Anwesh43 <anweshthecool0@gmail.com>
2024-09-21 11:23:55 +0530

    lineslantrightview: adding Renderer class

--- lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
+++ lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
@@ -185,4 +185,27 @@ class LineSlantRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineSlantRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lsr : LineSlantRight = LineSlantRight(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lsr.draw(canvas, paint)
+            animator.animate {
+                lsr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lsr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
