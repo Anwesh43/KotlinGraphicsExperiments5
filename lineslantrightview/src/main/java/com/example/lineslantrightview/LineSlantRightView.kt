eac8bc6d2cce650af05d76df31ff13b31a957330
Anwesh43 <anweshthecool0@gmail.com>
2024-09-21 11:14:28 +0530

    lineslantrightview: adding LSRNode class

--- lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
+++ lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
@@ -121,4 +121,45 @@ class LineSlantRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LSRNode? = null
+        private var prev : LSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSRNode {
+            var curr : LSRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
