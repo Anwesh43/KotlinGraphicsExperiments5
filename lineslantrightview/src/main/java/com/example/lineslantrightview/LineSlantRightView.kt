ef35efe26f4243fbfc8b14cb383b23f310c2936b
Anwesh43 <anweshthecool0@gmail.com>
2024-09-21 11:29:49 +0530

    lineslantrightview: adding fix

--- lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
+++ lineslantrightview/src/main/java/com/example/lineslantrightview/LineSlantRightView.kt
@@ -56,6 +56,7 @@ fun Canvas.drawLSRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineSlantRight(scale, w, h, paint)
 }
 
