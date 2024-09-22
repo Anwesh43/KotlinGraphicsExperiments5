74daf4e8c5428d9c00d15ede5bbb5eed101af65c
Anwesh43 <anweshthecool0@gmail.com>
2024-09-22 12:50:33 +0530

    arcconcverticalrotview: adding ACVRNode class

--- arcconcverticalrotview/src/main/java/com/example/arcconcverticalrotview/ArcConvVerticalRotView.kt
+++ arcconcverticalrotview/src/main/java/com/example/arcconcverticalrotview/ArcConvVerticalRotView.kt
@@ -124,4 +124,45 @@ class ArcConcVerticalRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ACVRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ACVRNode? = null
+        private var prev : ACVRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ACVRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACVRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACVRNode {
+            var curr : ACVRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
