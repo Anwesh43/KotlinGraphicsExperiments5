e10d828ea0d524dfcc57573a22a1c1877ffea892
Anwesh43 <anweshthecool0@gmail.com>
2024-09-22 13:06:11 +0530

    arcconcverticalrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcconcverticalrotview.ArcConcVerticalRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.lineslantrightview.LineSlantRightView
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineSlantRightView.create(this)
+        ArcConcVerticalRotView.create(this)
     }
 }
