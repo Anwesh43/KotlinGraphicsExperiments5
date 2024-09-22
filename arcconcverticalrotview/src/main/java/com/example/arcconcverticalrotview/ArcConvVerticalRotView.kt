883737d9578a4cd82da038f9547de62029a48ffb
Anwesh43 <anweshthecool0@gmail.com>
2024-09-22 12:28:04 +0530

    arcconcverticalrotview: adding Canvas extension methods

--- arcconcverticalrotview/src/main/java/com/example/arcconcverticalrotview/ArcConvVerticalRotView.kt
+++ arcconcverticalrotview/src/main/java/com/example/arcconcverticalrotview/ArcConvVerticalRotView.kt
@@ -27,3 +27,37 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcConcVerticalRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = size / 2
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, -size + size * j) {
+                val dsc1 : Float = dsc(0).divideScale(j, 2)
+                val dsc2 : Float = dsc(1).divideScale(1 - j, 2)
+                drawArc(RectF(0f, 0f, size, size), 90f, 180f * (dsc1 + dsc2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawACVRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcConcVerticalRot(scale, w, h, paint)
+}
