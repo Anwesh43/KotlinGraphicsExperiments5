d0c59e6c8c52fcd9715241283286722334d3fa8d
Anwesh43 <anweshthecool0@gmail.com>
2024-09-23 09:17:41 +0530

    bilinequarterarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,11 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcconcverticalrotview.ArcConcVerticalRotView
+import com.example.bilinequarterarcview.BiLineQuarterArcView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcConcVerticalRotView.create(this)
+        BiLineQuarterArcView.create(this)
     }
 }
