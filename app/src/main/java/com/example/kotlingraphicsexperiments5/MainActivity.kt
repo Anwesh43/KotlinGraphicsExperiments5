a414d4dcc3ee24ab910872108c8e5d63c4f8aebc
Anwesh43 <anweshthecool0@gmail.com>
2024-09-25 12:16:02 +0530

    linearcrightdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,11 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bilinequarterarcview.BiLineQuarterArcView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linearcrightdownview.LineArcRightDownView
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiLineQuarterArcView.create(this)
+        LineArcRightDownView.create(this)
     }
 }
