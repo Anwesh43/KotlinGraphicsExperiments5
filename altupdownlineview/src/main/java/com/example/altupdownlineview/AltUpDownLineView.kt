1b17707ef9a09e43f24ffb2598cba79e6c5dbfbb
Anwesh43 <anweshthecool0@gmail.com>
2024-09-26 12:42:30 +0530

    altupdownlienview: adding AltUpDownLine class

--- altupdownlineview/src/main/java/com/example/altupdownlineview/AltUpDownLineView.kt
+++ altupdownlineview/src/main/java/com/example/altupdownlineview/AltUpDownLineView.kt
@@ -163,4 +163,27 @@ class AltUpDownLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AltUpDownLine(var i : Int) {
+
+        private var curr : AUDLNode = AUDLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
