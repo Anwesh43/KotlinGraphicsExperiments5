96017148fc1ed13c4945eb1094c003f473f808e8
Anwesh43 <anweshthecool0@gmail.com>
2024-09-26 12:05:51 +0530

    altupdownlienview: adding AltUpDownLineView class

--- altupdownlineview/src/main/java/com/example/altupdownlineview/AltUpDownLineView.kt
+++ altupdownlineview/src/main/java/com/example/altupdownlineview/AltUpDownLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawAUDLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawAltUpDownLine(scale, w, h, paint)
 }
+
+class AltUpDownLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
