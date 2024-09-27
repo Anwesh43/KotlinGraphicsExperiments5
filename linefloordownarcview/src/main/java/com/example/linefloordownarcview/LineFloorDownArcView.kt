8aac4f161c2167f8e41daa5c1ce74068cc58e708
Anwesh43 <anweshthecool0@gmail.com>
2024-09-27 13:20:06 +0530

    linefloordownarcview: adding Canvas extension methods

--- linefloordownarcview/src/main/java/com/example/linefloordownarcview/LineFloorDownArcView.kt
+++ linefloordownarcview/src/main/java/com/example/linefloordownarcview/LineFloorDownArcView.kt
@@ -27,3 +27,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineFloorDownArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 90f * dsc(2), true, paint)
+    }
+}
+
+fun Canvas.drawLFDANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineFloorDownArc(scale, w, h, paint)
+}
