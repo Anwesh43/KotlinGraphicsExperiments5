cd49512172b535ba3f6dce92f93cbd60888f8b67
Anwesh43 <anweshthecool0@gmail.com>
2024-09-28 13:21:59 +0530

    linejoirotarcview: adding Canvas extension methods

--- linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
+++ linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineJoinRotArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * 2 * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(1) * j)
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+        drawLine(size, 0f, size * (1 - dsc(2)), size * dsc(2), paint)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, rot * dsc(1), true, paint)
+    }
+}
+
+fun Canvas.drawLJRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineJoinRotArc(scale, w, h, paint)
+}
