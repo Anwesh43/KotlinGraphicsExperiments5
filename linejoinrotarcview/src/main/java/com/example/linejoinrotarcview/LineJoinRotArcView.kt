9d9fa6e309e59ce1c8fd974b662115f9f5eb0bb5
Anwesh43 <anweshthecool0@gmail.com>
2024-09-28 13:26:21 +0530

    linejoirotarcview: adding LineJoinRotArcView class

--- linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
+++ linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
@@ -61,3 +61,17 @@ fun Canvas.drawLJRANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineJoinRotArc(scale, w, h, paint)
 }
+
+class LineJoinRotArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+
+        }
+        return true
+    }
+}
