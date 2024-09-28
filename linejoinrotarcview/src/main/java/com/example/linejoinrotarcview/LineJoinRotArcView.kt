12509b6458cd5d06e56c917db1b74a34a8374c30
Anwesh43 <anweshthecool0@gmail.com>
2024-09-28 13:41:06 +0530

    linejoirotarcview: adding Renderer in View

--- linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
+++ linejoinrotarcview/src/main/java/com/example/linejoinrotarcview/LineJoinRotArcView.kt
@@ -64,13 +64,17 @@ fun Canvas.drawLJRANode(i : Int, scale : Float, paint : Paint) {
 
 class LineJoinRotArcView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
-
+            MotionEvent.ACTION_DOWN -> {
+                renderer.handleTap()
+            }
         }
         return true
     }
