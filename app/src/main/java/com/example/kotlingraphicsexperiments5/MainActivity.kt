d18568d464f45819e23b840726fb7e32a16f58e4
Anwesh43 <anweshthecool0@gmail.com>
2024-09-28 13:42:41 +0530

    linejoirotarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linefloordownarcview.LineFloorDownArcView
+import com.example.linejoinrotarcview.LineJoinRotArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineFloorDownArcView.create(this)
+        LineJoinRotArcView.create(this)
     }
 }
