a1f6d10a427293a45b3d8b7aaa10bed524ecfdc0
Anwesh43 <anweshthecool0@gmail.com>
2024-09-29 12:55:04 +0530

    arcrotupdownview: adding ARUDNode class

--- arcrotupdownview/src/main/java/com/example/arcrotupdownview/ArcRotUpDownView.kt
+++ arcrotupdownview/src/main/java/com/example/arcrotupdownview/ArcRotUpDownView.kt
@@ -119,4 +119,45 @@ class ArcRotUpDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARUDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ARUDNode? = null
+        private var prev : ARUDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARUDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARUDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARUDNode {
+            var curr : ARUDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
