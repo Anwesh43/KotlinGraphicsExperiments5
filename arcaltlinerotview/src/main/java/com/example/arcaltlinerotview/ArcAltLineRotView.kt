0d2662c1dc56efd1367e4775d24507811afb0fdf
Anwesh43 <anweshthecool0@gmail.com>
2024-10-02 11:50:00 +0530

    arclinerotview: adding AALRNode class

--- arcaltlinerotview/src/main/java/com/example/arcaltlinerotview/ArcAltLineRotView.kt
+++ arcaltlinerotview/src/main/java/com/example/arcaltlinerotview/ArcAltLineRotView.kt
@@ -128,4 +128,45 @@ class ArcAltLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AALRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AALRNode? = null
+        private var prev : AALRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AALRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAALRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AALRNode {
+            var curr : AALRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
