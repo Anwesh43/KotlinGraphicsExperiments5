8850b27dc502b7f1ddc458ceb76f45ffc89dfc90
Anwesh43 <anweshthecool0@gmail.com>
2024-10-02 11:35:45 +0530

    arclinerotview: adding Canvas extension methods

--- arcaltlinerotview/src/main/java/com/example/arcaltlinerotview/ArcAltLineRotView.kt
+++ arcaltlinerotview/src/main/java/com/example/arcaltlinerotview/ArcAltLineRotView.kt
@@ -27,3 +27,41 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcAltLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float  ={
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY((w * 0.5f * dsc(3)), 0f) {
+                    drawXY(0f, 0f) {
+                        rotate(rot * dsc(2))
+                        drawLine(0f, -size, 0f, -size + size * 0.5f * dsc(0), paint)
+                    }
+                    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 90f * dsc(1), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawAALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcAltLineRot(scale, w, h, paint)
+}
