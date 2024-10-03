ad66e5fe7d5a342ef1229b8684c7286cec4e5372
Anwesh43 <anweshthecool0@gmail.com>
2024-10-03 18:11:59 +0530

    rightarcanglejoinview: adding RAAJNode class

--- rightarcanglejoinview/src/main/java/com/example/rightarcanglejoinview/RightArcAngleJoinView.kt
+++ rightarcanglejoinview/src/main/java/com/example/rightarcanglejoinview/RightArcAngleJoinView.kt
@@ -122,4 +122,45 @@ class RightArcAngleJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RAAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RAAJNode? = null
+        private var prev : RAAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RAAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRAAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RAAJNode {
+            var curr : RAAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
