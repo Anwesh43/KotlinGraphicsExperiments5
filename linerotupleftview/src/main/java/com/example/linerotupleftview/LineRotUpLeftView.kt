71fdffcf4a5338ac62e2de76d719d5f878f1c069
Anwesh43 <anweshthecool0@gmail.com>
2024-10-04 17:51:03 +0530

    linerotupleftview: adding LRULNode class

--- linerotupleftview/src/main/java/com/example/linerotupleftview/LineRotUpLeftView.kt
+++ linerotupleftview/src/main/java/com/example/linerotupleftview/LineRotUpLeftView.kt
@@ -120,4 +120,45 @@ class LineRotUpLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRULNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRULNode? = null
+        private var prev : LRULNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRULNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRULNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRULNode {
+            var curr : LRULNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
