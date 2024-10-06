7d61657a5f154a6ccb44de28c46726358357f32a
Anwesh43 <anweshthecool0@gmail.com>
2024-10-06 13:08:04 +0530

    linetriarccloseview: adding LTACNode class

--- linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
+++ linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
@@ -129,4 +129,45 @@ class LineTriArcCloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTACNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LTACNode? = null
+        private var next : LTACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTACNode {
+            var curr : LTACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
