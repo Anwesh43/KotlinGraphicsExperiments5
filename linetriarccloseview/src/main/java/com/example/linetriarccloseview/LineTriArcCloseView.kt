d2f11d7989ebf0dcc45f295a1f8e217999c05ccb
Anwesh43 <anweshthecool0@gmail.com>
2024-10-06 12:59:49 +0530

    linetriarccloseview: adding LineTriArcCloseView class

--- linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
+++ linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawLTACNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineTriArcClose(scale, w, h, paint)
 }
+
+class LineTriArcCloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
