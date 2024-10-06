327eac8254175b640896ccd93d1fa84f78f9b30e
Anwesh43 <anweshthecool0@gmail.com>
2024-10-06 13:19:44 +0530

    linetriarccloseview: adding LineTriArcClose class

--- linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
+++ linetriarccloseview/src/main/java/com/example/linetriarccloseview/LineTriArcCloseView.kt
@@ -170,4 +170,27 @@ class LineTriArcCloseView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineTriArcClose(var i : Int) {
+
+        private var curr : LTACNode = LTACNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
