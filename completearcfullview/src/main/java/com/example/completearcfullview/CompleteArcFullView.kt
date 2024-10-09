9c5186cca91c35506bf8b588a4e823ddc9121d2e
Anwesh43 <anweshthecool0@gmail.com>
2024-10-09 12:43:30 +0530

    completearcfullview: adding Canvas extension methods

--- completearcfullview/src/main/java/com/example/completearcfullview/CompleteArcFullView.kt
+++ completearcfullview/src/main/java/com/example/completearcfullview/CompleteArcFullView.kt
@@ -27,3 +27,39 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCompleteArcFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2), h / 2 - (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            paint.style = Paint.Style.STROKE
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(1) * (1 - j))
+                drawArc(RectF(-size, -size / 2, 0f, size / 2), 180f * j, 180f * dsc(0).divideScale(j, 2), false, paint)
+            }
+            paint.style = Paint.Style.FILL
+            drawXY(-size + size * j, 0f) {
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(2).divideScale(j, 2), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawCAFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCompleteArcFull(scale, w, h, paint)
+}
