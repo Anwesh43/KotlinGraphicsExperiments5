fca3fa5dba59f1e251865c9b0378d63e296e8b91
Anwesh43 <anweshthecool0@gmail.com>
2024-10-09 13:58:20 +0530

    completearcfullview: adding static create method

--- completearcfullview/src/main/java/com/example/completearcfullview/CompleteArcFullView.kt
+++ completearcfullview/src/main/java/com/example/completearcfullview/CompleteArcFullView.kt
@@ -215,4 +215,13 @@ class CompleteArcFullView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : CompleteArcFullView {
+            val view : CompleteArcFullView = CompleteArcFullView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
