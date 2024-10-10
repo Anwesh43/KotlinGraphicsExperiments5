fb692fa4b61637375a293d5a6c98f2ce11a55d89
Anwesh43 <anweshthecool0@gmail.com>
2024-10-10 12:34:57 +0530

    linebreakdownview: adding LineBreakUpDownView class

--- linebreakupdownview/src/main/java/com/example/linebreakupdownview/LineBreakUpDownView.kt
+++ linebreakupdownview/src/main/java/com/example/linebreakupdownview/LineBreakUpDownView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLBUDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBreakUpDown(scale, w, h, paint)
 }
+
+class LineBreakUpDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
