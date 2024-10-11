4713dc082e5bbf3cab931a3479ad058744130081
Anwesh43 <anweshthecool0@gmail.com>
2024-10-11 18:14:04 +0530

    sidearcmiddownview: adding Renderer class

--- sidearcmiddownview/src/main/java/com/example/sidearcmiddownview/SideArcMidDownView.kt
+++ sidearcmiddownview/src/main/java/com/example/sidearcmiddownview/SideArcMidDownView.kt
@@ -193,4 +193,27 @@ class SideArcMidDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SideArcMidDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val samd : SideArcMidDown = SideArcMidDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            samd.draw(canvas, paint)
+            animator.animate {
+                samd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            samd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
