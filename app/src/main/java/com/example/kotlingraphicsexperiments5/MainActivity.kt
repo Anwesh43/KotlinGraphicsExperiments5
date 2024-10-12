794fc65faf271b36e02b017732421e88883528f4
Anwesh43 <anweshthecool0@gmail.com>
2024-10-12 13:03:22 +0530

    lineshiftrotlineview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sidearcmiddownview.SideArcMidDownView
+import com.example.lineshiftrotlineview.LineShiftRotLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SideArcMidDownView.create(this)
+        LineShiftRotLineView.create(this)
     }
 }
