ea0b416ceab70e1e6f9ffa0f46ce04dc0a22abdb
Anwesh43 <anweshthecool0@gmail.com>
2024-10-13 11:51:37 +0530

    linerotholderupview: adding LineRotHolderUp class

--- linerotholderupview/src/main/java/com/example/linerotholderupview/LineRotHolderUpView.kt
+++ linerotholderupview/src/main/java/com/example/linerotholderupview/LineRotHolderUpView.kt
@@ -164,4 +164,27 @@ class LineRotHolderUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotHolderUp(var i : Int) {
+
+        private var curr : LRHUNode = LRHUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
