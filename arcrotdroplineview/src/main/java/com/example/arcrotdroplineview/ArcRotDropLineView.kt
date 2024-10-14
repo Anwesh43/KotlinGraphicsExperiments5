1663c72fec42400e34eb3e805f4a382c2b855d71
Anwesh43 <anweshthecool0@gmail.com>
2024-10-14 11:40:21 +0530

    arcrotdroplineview: adding ARDLNode class

--- arcrotdroplineview/src/main/java/com/example/arcrotdroplineview/ArcRotDropLineView.kt
+++ arcrotdroplineview/src/main/java/com/example/arcrotdroplineview/ArcRotDropLineView.kt
@@ -122,4 +122,45 @@ class ArcRotDropLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARDLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ARDLNode? = null
+        private var prev : ARDLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARDLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARDLNode {
+            var curr : ARDLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
