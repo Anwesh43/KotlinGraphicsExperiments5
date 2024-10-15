de072e0cfcc9f3306aeacc8a88fa8fb9000161d6
Anwesh43 <anweshthecool0@gmail.com>
2024-10-15 11:24:36 +0530

    arcaltsiderotview: adding module setup

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.arcrotdroplineview.ArcRotDropLineView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
