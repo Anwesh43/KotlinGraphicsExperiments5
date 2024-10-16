d8b329ef4f84c875079729b2f68504cc6142fa28
Anwesh43 <anweshthecool0@gmail.com>
2024-10-16 14:10:00 +0530

    trilinecloseupview: adding fixes

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcaltsiderotview.ArcAltSideRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.trilinecloseupview.TriLineCloseUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcAltSideRotView.create(this)
+        TriLineCloseUpView.create(this)
     }
 }
