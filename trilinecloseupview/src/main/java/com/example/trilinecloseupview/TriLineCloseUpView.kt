5bc09ccfa8f41f770bd190bdd9e42feb87c0fdac
Anwesh43 <anweshthecool0@gmail.com>
2024-10-16 12:40:16 +0530

    trilinecloseupview: adding TriLineCloseUpView class

--- trilinecloseupview/src/main/java/com/example/trilinecloseupview/TriLineCloseUpView.kt
+++ trilinecloseupview/src/main/java/com/example/trilinecloseupview/TriLineCloseUpView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawTLCUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriLineCloseUp(scale, w, h, paint)
 }
+
+class TriLineCloseUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
