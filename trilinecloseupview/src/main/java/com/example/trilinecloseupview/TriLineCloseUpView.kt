4223c3f75c381d1582ad449bebdd560fe5848bab
Anwesh43 <anweshthecool0@gmail.com>
2024-10-16 12:34:27 +0530

    trilinecloseupview: adding Canvas extension methods

--- trilinecloseupview/src/main/java/com/example/trilinecloseupview/TriLineCloseUpView.kt
+++ trilinecloseupview/src/main/java/com/example/trilinecloseupview/TriLineCloseUpView.kt
@@ -27,3 +27,40 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriLineCloseUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+        drawXY(-size, 0f) {
+            val ds1 : Float = dsc(0).divideScale(0, 2)
+            val ds2 : Float = dsc(1).divideScale(1, 2)
+            drawLine(0f, 0f, size * 0.5f * ds1, -size * 0.5f * ds1, paint)
+            drawXY(size / 2, -size / 2) {
+                drawLine(0f, 0f, size * 0.5f * ds2, size * 0.5f * ds2, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTLCUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriLineCloseUp(scale, w, h, paint)
+}
