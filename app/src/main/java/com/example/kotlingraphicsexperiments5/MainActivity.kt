87673ad59a0b7f03bcf13dcf3bde4e42e1b672cc
Anwesh43 <anweshthecool0@gmail.com>
2024-10-17 13:07:12 +0530

    linemultirotleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.trilinecloseupview.TriLineCloseUpView
-
+import com.example.linemultirotleftview.LineMultiRotLeftView
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TriLineCloseUpView.create(this)
+        LineMultiRotLeftView.create(this)
     }
 }
