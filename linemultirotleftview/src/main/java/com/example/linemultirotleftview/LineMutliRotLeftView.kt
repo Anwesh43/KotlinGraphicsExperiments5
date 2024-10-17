0be862c414fdbd1e7d1c37d356ce3b9a7815d3dc
Anwesh43 <anweshthecool0@gmail.com>
2024-10-17 12:59:45 +0530

    linemultirotleftview: adding Renderer class

--- linemultirotleftview/src/main/java/com/example/linemultirotleftview/LineMutliRotLeftView.kt
+++ linemultirotleftview/src/main/java/com/example/linemultirotleftview/LineMutliRotLeftView.kt
@@ -186,4 +186,27 @@ class LineMultiRotLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineMultiRotLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val lmrl : LineMultiRotLeft = LineMultiRotLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lmrl.draw(canvas, paint)
+            animator.animate {
+                lmrl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lmrl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
