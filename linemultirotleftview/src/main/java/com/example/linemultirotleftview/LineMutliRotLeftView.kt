9788c5d5fa963d463e53d519d3b82b812a66dca4
Anwesh43 <anweshthecool0@gmail.com>
2024-10-17 12:21:07 +0530

    linemultirotleftview: adding LineMultiRotLeftView class

--- linemultirotleftview/src/main/java/com/example/linemultirotleftview/LineMutliRotLeftView.kt
+++ linemultirotleftview/src/main/java/com/example/linemultirotleftview/LineMutliRotLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLMRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineMutliRotLeft(scale, w, h, paint)
 }
+
+class LineMultiRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
