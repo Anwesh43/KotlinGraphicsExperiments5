e94642ea65306e43530945c1ff1e15aef9b315d3
Anwesh43 <anweshthecool0@gmail.com>
2024-10-18 12:34:59 +0530

    openlinestrokerotview: adding Canvas extension methods

--- openlinestrokerotview/src/main/java/com/example/openlinestrokerotview/OpenLineStrokeRotView.kt
+++ openlinestrokerotview/src/main/java/com/example/openlinestrokerotview/OpenLineStrokeRotView.kt
@@ -26,3 +26,36 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawOpenLineStrokeRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawXY(0f, -size) {
+            drawLine(0f, 0f, -size * dsc(1).divideScale(0, 2), 0f, paint)
+        }
+        drawXY(-size, -size) {
+            drawLine(0f, 0f, 0f, size * dsc(1).divideScale(1, 2), paint)
+        }
+    }
+}
+
+fun Canvas.drawOLSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOpenLineStrokeRot(scale, w, h, paint)
+}
