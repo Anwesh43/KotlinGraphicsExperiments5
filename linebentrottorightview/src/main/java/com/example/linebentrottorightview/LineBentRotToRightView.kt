d6f74c6832e1e5e24fb55ab0a71f6ac9a11925b4
Anwesh43 <anweshthecool0@gmail.com>
2024-10-19 12:55:10 +0530

    linebentrottorightview: adding LineBentRotToRightView class

--- linebentrottorightview/src/main/java/com/example/linebentrottorightview/LineBentRotToRightView.kt
+++ linebentrottorightview/src/main/java/com/example/linebentrottorightview/LineBentRotToRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLBRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentRotToRight(scale, w, h, paint)
 }
+
+class LineBentRotToRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
