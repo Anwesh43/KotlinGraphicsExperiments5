4b0cdc32bd8e3e8edf3ece7aa6df2d4ef5a62e7a
Anwesh43 <anweshthecool0@gmail.com>
2024-10-20 13:26:58 +0530

    linedividearccontainview: adding LDACNode class

--- linedividearccontainview/src/main/java/com/example/linedividearccontainview/LineDivideArcContainView.kt
+++ linedividearccontainview/src/main/java/com/example/linedividearccontainview/LineDivideArcContainView.kt
@@ -128,4 +128,45 @@ class LineDivideArcContainView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDACNode? = null
+        private var prev : LDACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDACNode {
+            var curr : LDACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
