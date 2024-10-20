5fb5efcefb367a12ed72ea7bfa3847c74e189986
Anwesh43 <anweshthecool0@gmail.com>
2024-10-20 13:35:50 +0530

    linedividearccontainview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebentrottorightview.LineBentRotToRightView
+import com.example.linedividearccontainview.LineDivideArcContainView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentRotToRightView.create(this)
+        LineDivideArcContainView.create(this)
     }
 }
