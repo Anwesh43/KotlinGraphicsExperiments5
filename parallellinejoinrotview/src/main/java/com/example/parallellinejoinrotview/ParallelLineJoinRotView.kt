ba0b8b5fa123581631774ebc8153059aa9a3285f
Anwesh43 <anweshthecool0@gmail.com>
2024-10-21 13:21:54 +0530

    parallellinejoinrotview: adding ParallelLineJoinRotView class

--- parallellinejoinrotview/src/main/java/com/example/parallellinejoinrotview/ParallelLineJoinRotView.kt
+++ parallellinejoinrotview/src/main/java/com/example/parallellinejoinrotview/ParallelLineJoinRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawPLJRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawParallelLineJoinRot(scale, w, h, paint)
 }
+
+class ParallelLineJoinRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
