9eb470dc880e9a14368dbd8c7bdfb770a7c2c8b3
Anwesh43 <anweshthecool0@gmail.com>
2024-10-23 16:29:13 +0530

    linebentmorearcview: adding Renderer in View

--- linebentmorearcview/src/main/java/com/example/linebentmorearcview/LineBentMoreArcView.kt
+++ linebentmorearcview/src/main/java/com/example/linebentmorearcview/LineBentMoreArcView.kt
@@ -68,14 +68,16 @@ fun Canvas.drawLBMANode(i : Int, scale : Float, paint : Paint) {
 
 class LineBentMoreArcView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
