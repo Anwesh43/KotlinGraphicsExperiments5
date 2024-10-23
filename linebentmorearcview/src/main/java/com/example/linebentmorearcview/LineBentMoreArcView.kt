8d3f9f52ab60c109caa6977ffb961afe75efcb58
Anwesh43 <anweshthecool0@gmail.com>
2024-10-23 16:33:41 +0530

    linebentmorearcview: adding fix

--- linebentmorearcview/src/main/java/com/example/linebentmorearcview/LineBentMoreArcView.kt
+++ linebentmorearcview/src/main/java/com/example/linebentmorearcview/LineBentMoreArcView.kt
@@ -40,7 +40,7 @@ fun Canvas.drawLineBentMoreArc(scale : Float, w : Float, h : Float, paint : Pain
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
         drawXY(0f, 0f) {
             rotate(rot * dsc(1))
             drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
