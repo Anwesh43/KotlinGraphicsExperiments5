a55a0bc565238b9e644bb4456025a888e85d3bee
Anwesh43 <anweshthecool0@gmail.com>
2024-10-24 15:48:49 +0530

    arcfromrightupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcfromrightupview.ArcFromRightUpView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebentmorearcview.LineBentMoreArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentMoreArcView.create(this)
+        ArcFromRightUpView.create(this)
     }
 }
