dd87aa93b4bc4e599af60f2228a5ca273d007d27
Anwesh43 <anweshthecool0@gmail.com>
2024-10-24 13:37:56 +0530

    arcfromrightupview: adding Canvas extension methods

--- arcfromrightupview/src/main/java/com/example/arcfromrightupview/ArcFromRightUpView.kt
+++ arcfromrightupview/src/main/java/com/example/arcfromrightupview/ArcFromRightUpView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,32 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcFromRightUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
+        drawXY((w / 2 - size) * (1 - dsc(1)), 0f) {
+            drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * (dsc(0) + dsc(3)), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawAFRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+
+}
