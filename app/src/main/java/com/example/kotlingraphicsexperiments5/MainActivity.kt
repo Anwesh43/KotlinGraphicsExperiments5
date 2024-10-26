028162b7db011795d97a934111dd77515edaf3fb
Anwesh43 <anweshthecool0@gmail.com>
2024-10-26 12:24:42 +0530

    linealtrotrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.baruprotdownview.BarRotUpDownView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linealtrotrightview.LineAltRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BarRotUpDownView.create(this)
+        LineAltRotRightView.create(this)
     }
 }
