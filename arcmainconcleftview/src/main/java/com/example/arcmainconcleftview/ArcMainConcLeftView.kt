5119287f7b65a229711e4c28df4550605c046203
Anwesh43 <anweshthecool0@gmail.com>
2024-10-27 14:28:41 +0530

    arcmainconcleftview: adding ArcMainConcLeftView class

--- arcmainconcleftview/src/main/java/com/example/arcmainconcleftview/ArcMainConcLeftView.kt
+++ arcmainconcleftview/src/main/java/com/example/arcmainconcleftview/ArcMainConcLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawAMCLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcMainConcLeft(scale, w, h, paint)
 }
+
+class ArcMainConcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
