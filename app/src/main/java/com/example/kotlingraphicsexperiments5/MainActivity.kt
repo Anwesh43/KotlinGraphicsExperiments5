90bbd028bec713ad627d95363458f37b2ee30562
Anwesh43 <anweshthecool0@gmail.com>
2024-10-27 15:18:05 +0530

    arcmainconcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcmainconcleftview.ArcMainConcLeftView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linealtrotrightview.LineAltRotRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineAltRotRightView.create(this)
+        ArcMainConcLeftView.create(this)
     }
 }
