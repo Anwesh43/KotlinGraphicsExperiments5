6c7eb9a6f501ffd133c0ea672deb3fead4f18228
Anwesh43 <anweshthecool0@gmail.com>
2024-10-28 15:36:14 +0530

    linemultirotrightview: adding Canvas extension methods

--- linemultirotrightview/src/main/java/com/example/linemultirotrightview/LineMultiRotRightView.kt
+++ linemultirotrightview/src/main/java/com/example/linemultirotrightview/LineMultiRotRightView.kt
@@ -26,3 +26,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineMultiRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(size + (w / 2) * (1 - dsc(0)), 0f) {
+                rotate(rot * dsc(1))
+                drawLine(-size, 0f, 0f, 0f, paint)
+            }
+        }
+        drawXY(size + (w / 2) * (1 - dsc(2)), -size) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLMRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineMultiRotRight(scale, w, h, paint)
+}
