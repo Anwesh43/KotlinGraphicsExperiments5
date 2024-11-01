d4228d31b5089e1a093b7a4f4c1f982ec6f6764c
Anwesh43 <anweshthecool0@gmail.com>
2024-11-01 13:57:04 +0530

    rotsqleftexpandview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linemultirotrightview.LineMultiRotRightView
+import com.example.rotsqleftexpandview.RotSqLeftExpandView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineMultiRotRightView.create(this)
+        RotSqLeftExpandView.create(this)
     }
 }
