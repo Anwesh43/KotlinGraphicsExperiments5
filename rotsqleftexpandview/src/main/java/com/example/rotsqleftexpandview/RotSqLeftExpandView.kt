fd862f4fd61d05f5184c0937ba76f67eef9e3fe8
Anwesh43 <anweshthecool0@gmail.com>
2024-11-01 13:41:21 +0530

    rotsqleftexpandview: adding RotSqLeftExpandView class

--- rotsqleftexpandview/src/main/java/com/example/rotsqleftexpandview/RotSqLeftExpandView.kt
+++ rotsqleftexpandview/src/main/java/com/example/rotsqleftexpandview/RotSqLeftExpandView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawRSENode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawRotSqExpand(scale, w, h, paint)
 }
+
+class RotSqLeftExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
