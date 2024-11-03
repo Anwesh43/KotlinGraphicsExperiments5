1944fb1f995d6dd5535a352a9806a728164903dc
Anwesh43 <anweshthecool0@gmail.com>
2024-11-03 13:50:49 +0530

    bentlineeithersideview: adding BLESNode class

--- bentlineeithersideview/src/main/java/com/example/bentlineeithersideview/BentLineEitherSideView.kt
+++ bentlineeithersideview/src/main/java/com/example/bentlineeithersideview/BentLineEitherSideView.kt
@@ -125,4 +125,45 @@ class BentLineEitherSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLESNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLESNode? = null
+        private var prev : BLESNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLESNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLESNode {
+            var curr : BLESNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
