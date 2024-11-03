90feca92417b09680829735d3a1256e2bc5e9dfe
Anwesh43 <anweshthecool0@gmail.com>
2024-11-03 14:03:34 +0530

    bentlineeithersideview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bentlineeithersideview.BentLineEitherSideView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.opensqrotrightview.OpenSqRotRightView
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        OpenSqRotRightView.create(this)
+        BentLineEitherSideView.create(this)
     }
 }
