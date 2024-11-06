f73bebcd8929a9366237c51856508e4ab50430db
Anwesh43 <anweshthecool0@gmail.com>
2024-11-06 17:38:19 +0530

    linebentsqrotview: adding LineBentSqRotView class

--- linebentsqrotview/src/main/java/com/example/linebentsqrotview/LineBentSqRotView.kt
+++ linebentsqrotview/src/main/java/com/example/linebentsqrotview/LineBentSqRotView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentSqRot(scale, w, h, paint)
 }
+
+class LineBentSqRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
