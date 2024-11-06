c9406b7ddc1f30eb5070cc108a4b71f82cd5f11c
Anwesh43 <anweshthecool0@gmail.com>
2024-11-06 21:08:12 +0530

    linebentsqrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,11 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bentlineeithersideview.BentLineEitherSideView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linebentsqrotview.LineBentSqRotView
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineEitherSideView.create(this)
+        LineBentSqRotView.create(this)
     }
 }
