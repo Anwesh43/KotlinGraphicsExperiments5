c08d9afcc37c2a7b1eccd92a0f4ea47b331e45f8
Anwesh43 <anweshthecool0@gmail.com>
2024-11-07 14:09:12 +0530

    linedropbreakrightview: adding fixes

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebentsqrotview.LineBentSqRotView
+import com.example.linedropbreakrightview.LineDropBreakRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentSqRotView.create(this)
+        LineDropBreakRightView.create(this)
     }
 }
