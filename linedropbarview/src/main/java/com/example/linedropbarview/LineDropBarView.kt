ac0f3b54b614dc6d1b4271bc19ec6f06d437daad
Anwesh43 <anweshthecool0@gmail.com>
2024-11-08 12:55:00 +0530

    linedropbarview: adding Canvas extension methods

--- linedropbarview/src/main/java/com/example/linedropbarview/LineDropBarView.kt
+++ linedropbarview/src/main/java/com/example/linedropbarview/LineDropBarView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,36 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDropBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val barW : Float = size / 3
+    drawXY(w / 2, h / 2) {
+        drawXY(size, -h * 0.5f * (1 - dsc(1)) + h * 0.5f * dsc(3)) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-barW, -size, 0f, 0f), paint)
+        }
+        drawXY((w / 2) * dsc(4), 0f) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLDBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDropBar(scale, w, h, paint)
+}
