7f2c15878cb381e57ceb059646ec382d8c179a32
Anwesh43 <anweshthecool0@gmail.com>
2024-11-09 22:32:40 +0530

    arcextendlinedropview: adding Renderer class

--- arcextendlinedropview/src/main/java/com/example/arcextendlinedropview/ArcExtendLineDropView.kt
+++ arcextendlinedropview/src/main/java/com/example/arcextendlinedropview/ArcExtendLineDropView.kt
@@ -186,4 +186,27 @@ class ArcExtendLineDropView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcExtendLineDropView) {
+
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val aeld : ArcExtendLineDrop = ArcExtendLineDrop(0)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            aeld.draw(canvas, paint)
+            animator.animate {
+                aeld.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            aeld.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
