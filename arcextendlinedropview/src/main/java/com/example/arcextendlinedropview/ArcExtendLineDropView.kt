cb56bb1dc677c1993e8c0d8afc4028df6af6b46a
Anwesh43 <anweshthecool0@gmail.com>
2024-11-09 22:25:57 +0530

    arcextendlinedropview: adding AELDNode class

--- arcextendlinedropview/src/main/java/com/example/arcextendlinedropview/ArcExtendLineDropView.kt
+++ arcextendlinedropview/src/main/java/com/example/arcextendlinedropview/ArcExtendLineDropView.kt
@@ -122,4 +122,45 @@ class ArcExtendLineDropView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AELDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AELDNode? = null
+        private var prev : AELDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AELDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAELDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AELDNode {
+            var curr : AELDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
