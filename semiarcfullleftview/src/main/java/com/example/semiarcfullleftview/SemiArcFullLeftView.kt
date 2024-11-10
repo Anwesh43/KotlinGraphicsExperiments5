9b46d2582fdbf313dce92de633adc240e72c3677
Anwesh43 <anweshthecool0@gmail.com>
2024-11-11 01:03:02 +0530

    semiarcfullleftview: adding SAFLNode class

--- semiarcfullleftview/src/main/java/com/example/semiarcfullleftview/SemiArcFullLeftView.kt
+++ semiarcfullleftview/src/main/java/com/example/semiarcfullleftview/SemiArcFullLeftView.kt
@@ -122,4 +122,45 @@ class SemiArcFullLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAFLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SAFLNode? = null
+        private var prev : SAFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SAFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAFLNode {
+            var curr : SAFLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
