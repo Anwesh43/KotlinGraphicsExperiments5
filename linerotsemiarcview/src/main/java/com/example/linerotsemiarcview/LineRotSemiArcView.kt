922a08e403c886af2a05ce6cbc6f377b0e9f0dc4
Anwesh43 <anweshthecool0@gmail.com>
2024-11-10 12:45:28 +0530

    linerotsemiarcview: adding Canvas extension methods

--- linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
+++ linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -28,3 +28,36 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotSemiArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2  - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, rot * dsc(0), false, paint)
+        }
+        drawXY(-w * 0.5f * (1 - dsc(2)), 0f) {
+            rotate(deg * dsc(3))
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRSANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSemiArc(scale, w, h, paint)
+}
