9927d29ce3b6126bd94650d62d62f9a6eaaa6be2
Anwesh43 <anweshthecool0@gmail.com>
2024-11-15 15:57:39 +0530

    semiarcrotlineview: adding SARLNode class

--- semiarcrotlineview/src/main/java/com/example/semiarcrotlineview/SemiArcRotLineView.kt
+++ semiarcrotlineview/src/main/java/com/example/semiarcrotlineview/SemiArcRotLineView.kt
@@ -120,4 +120,45 @@ class SemiArcRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SARLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SARLNode? = null
+        private var prev : SARLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size -1) {
+                next = SARLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSARLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SARLNode {
+            var curr : SARLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
