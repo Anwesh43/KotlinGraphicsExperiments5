d3f8197b4d697ec310a26c26cee76d8397730fef
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 12:33:22 +0530

    semiarctofullleftview: adding SAFUNode class

--- semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
+++ semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
@@ -118,4 +118,45 @@ class SemiArcToFullUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAFUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SAFUNode? = null
+        private var prev : SAFUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SAFUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAFUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun strtUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAFUNode {
+            var curr : SAFUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
