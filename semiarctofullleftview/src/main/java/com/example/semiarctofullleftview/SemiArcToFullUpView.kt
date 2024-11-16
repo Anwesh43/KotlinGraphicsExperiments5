7c75027049df076a717f22446ded537bab9877ab
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 10:15:06 +0530

    semiarctofullleftview: adding SemiArcToFullUpView class

--- semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
+++ semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawSAFUNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSemiArcToFullUp(scale, w, h, paint)
 }
+
+class SemiArcToFullUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+        }
+        return true
+    }
+}
