d85b32a296bef8e22b61304182a7aa94a986c256
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 12:41:09 +0530

    semiarctofullleftview: adding Renderer in View

--- semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
+++ semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
@@ -58,14 +58,16 @@ fun Canvas.drawSAFUNode(i : Int, scale : Float, paint : Paint) {
 
 class SemiArcToFullUpView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                return true
+                renderer.handleTap()
             }
         }
         return true
