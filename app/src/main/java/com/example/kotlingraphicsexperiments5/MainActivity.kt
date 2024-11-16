2418feb9fcf0bed64ffc99acb05a69fca3293cfd
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 12:44:38 +0530

    semiarctofullleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.semiarcrotlineview.SemiArcRotLineView
+import com.example.semiarctofullleftview.SemiArcToFullUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SemiArcRotLineView.create(this)
+        SemiArcToFullUpView.create(this)
     }
 }
