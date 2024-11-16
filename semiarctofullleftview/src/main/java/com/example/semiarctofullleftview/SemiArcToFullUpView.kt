d25d17988e28b88899c720ac697f481af898120b
Anwesh43 <anweshthecool0@gmail.com>
2024-11-16 12:37:40 +0530

    semiarctofullleftview: adding SemiArcToFullUp class

--- semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
+++ semiarctofullleftview/src/main/java/com/example/semiarctofullleftview/SemiArcToFullUpView.kt
@@ -143,7 +143,7 @@ class SemiArcToFullUpView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun strtUpdating(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -157,6 +157,29 @@ class SemiArcToFullUpView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class SemiArcToFullUp(var i : Int) {
+
+        private var curr : SAFUNode = SAFUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
