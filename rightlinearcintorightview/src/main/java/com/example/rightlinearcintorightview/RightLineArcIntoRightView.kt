037c789e01fe975dad0fadb15741eb03b743098b
Anwesh43 <anweshthecool0@gmail.com>
2024-11-17 12:54:20 +0530

    rightlinearcintorightview: adding Canvas extension methods

--- rightlinearcintorightview/src/main/java/com/example/rightlinearcintorightview/RightLineArcIntoRightView.kt
+++ rightlinearcintorightview/src/main/java/com/example/rightlinearcintorightview/RightLineArcIntoRightView.kt
@@ -27,3 +27,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineArcIntoRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size, 0) {
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+            drawXY(0f, size) {
+                rotate(rot * dsc(3))
+                drawArc(RectF(-size / 2, 0f, size / 2, size), 90f, 180f * dsc(2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRLAIRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor("#BDBDBD")
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineArcIntoRight(scale, w, h, paint)
+}
