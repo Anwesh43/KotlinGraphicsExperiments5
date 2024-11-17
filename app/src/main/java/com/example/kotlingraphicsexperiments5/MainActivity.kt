e17e3389e9d0620c829ec88402908350cb4d234e
Anwesh43 <anweshthecool0@gmail.com>
2024-11-17 13:35:09 +0530

    rightlinearcintorightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.semiarctofullleftview.SemiArcToFullUpView
+import com.example.rightlinearcintorightview.RightLineArcIntoRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SemiArcToFullUpView.create(this)
+        RightLineArcIntoRightView.create(this)
     }
 }
