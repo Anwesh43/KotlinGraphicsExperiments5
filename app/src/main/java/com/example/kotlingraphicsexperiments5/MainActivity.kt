6ef2167890d2cf4155f150afd7c92fd2442edbe3
Anwesh43 <anweshthecool0@gmail.com>
2024-11-22 19:16:20 +0530

    linerightjoinrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.rightlinearcintorightview.RightLineArcIntoRightView
+import com.example.linerightjoinrotview.LineRotJoinRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RightLineArcIntoRightView.create(this)
+        LineRotJoinRotView.create(this)
     }
 }
