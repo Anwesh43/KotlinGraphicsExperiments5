3ef663804394bab7ff836a6f46c3c516238fb2ac
Anwesh43 <anweshthecool0@gmail.com>
2024-11-27 18:45:36 +0530

    linebenttobentview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linerightjoinrotview.LineRotJoinRotView
+import com.example.linebenttobentview.LineBentToBentView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotJoinRotView.create(this)
+        LineBentToBentView.create(this)
     }
 }
