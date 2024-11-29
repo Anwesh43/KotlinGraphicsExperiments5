189382b7b805c924f854df6967fdea0a5d3d74a5
Anwesh43 <anweshthecool0@gmail.com>
2024-11-29 08:35:03 +0530

    linerotsideleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebenttobentview.LineBentToBentView
+import com.example.linerotsideleftview.LineRotSideLeftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentToBentView.create(this)
+        LineRotSideLeftView.create(this)
     }
 }
