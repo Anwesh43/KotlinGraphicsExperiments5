f4f80e6bd4b169584801c5108e64a22d55acd074
Anwesh43 <anweshthecool0@gmail.com>
2024-11-29 08:19:18 +0530

    linerotsideleftview: adding LineRotSideLeftView class

--- linerotsideleftview/src/main/java/com/example/linerotsideleftview/LineRotSideLeftView.kt
+++ linerotsideleftview/src/main/java/com/example/linerotsideleftview/LineRotSideLeftView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLRSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotSideLeft(scale, w, h, paint)
 }
+
+class LineRotSideLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
