de8fc9937b73eed739f48bcc129a574d3e81f954
Anwesh43 <anweshthecool0@gmail.com>
2024-12-01 04:23:13 +0530

    linerotjoinupview: adding LineRotJoinUpView class

--- linerotjoinupview/src/main/java/com/example/linerotjoinupview/LineRotJoinUpView.kt
+++ linerotjoinupview/src/main/java/com/example/linerotjoinupview/LineRotJoinUpView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawLRJUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotJoinUp(scale, w, h, paint)
 }
+
+class LineRotJoinUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
