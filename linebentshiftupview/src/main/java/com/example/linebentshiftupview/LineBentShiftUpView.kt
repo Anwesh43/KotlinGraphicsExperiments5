3f2956ec37e8c1c0bd80dbebb6827923d6ec6ad3
Anwesh43 <anweshthecool0@gmail.com>
2024-11-30 07:55:21 +0530

    linebentshiftupview: adding LineBentShiftUpView class

--- linebentshiftupview/src/main/java/com/example/linebentshiftupview/LineBentShiftUpView.kt
+++ linebentshiftupview/src/main/java/com/example/linebentshiftupview/LineBentShiftUpView.kt
@@ -55,3 +55,17 @@ fun Canvas.drawLBSUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentShiftUp(scale, w, h, paint)
 }
+
+class LineBentShiftUpView(ctx : Context) : View(ctx) {
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
