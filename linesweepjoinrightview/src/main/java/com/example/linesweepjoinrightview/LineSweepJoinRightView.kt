20d505a6ab69ecb9aabc286a3ccc7fbbfe4f6c65
Anwesh43 <anweshthecool0@gmail.com>
2024-12-04 11:51:01 +0530

    linesweepjoinrightview: adding fixes

--- linesweepjoinrightview/src/main/java/com/example/linesweepjoinrightview/LineSweepJoinRightView.kt
+++ linesweepjoinrightview/src/main/java/com/example/linesweepjoinrightview/LineSweepJoinRightView.kt
@@ -57,6 +57,7 @@ fun Canvas.drawLSJRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawLineSweepJoinRight(scale, w, h, paint)
 }
 
