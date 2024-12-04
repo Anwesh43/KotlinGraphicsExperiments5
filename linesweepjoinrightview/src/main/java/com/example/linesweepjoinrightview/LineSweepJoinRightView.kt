9f936cedcac6889c93257206ab8f10610f966cd7
Anwesh43 <anweshthecool0@gmail.com>
2024-12-04 10:10:10 +0530

    linesweepjoinrightview: adding LineSweepJoinRightView class

--- linesweepjoinrightview/src/main/java/com/example/linesweepjoinrightview/LineSweepJoinRightView.kt
+++ linesweepjoinrightview/src/main/java/com/example/linesweepjoinrightview/LineSweepJoinRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLSJRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSweepJoinRight(scale, w, h, paint)
 }
+
+class LineSweepJoinRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
