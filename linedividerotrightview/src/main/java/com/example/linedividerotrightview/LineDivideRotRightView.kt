119618fdc83849c7914c949fd05f8fa3abe5b825
Anwesh43 <anweshthecool0@gmail.com>
2024-12-05 09:21:44 +0530

    linedividerotrightview: adding LineDivideRotRightView class

--- linedividerotrightview/src/main/java/com/example/linedividerotrightview/LineDivideRotRightView.kt
+++ linedividerotrightview/src/main/java/com/example/linedividerotrightview/LineDivideRotRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLDRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineDivideRotRight(scale, w, h, paint)
 }
+
+class LineDivideRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
