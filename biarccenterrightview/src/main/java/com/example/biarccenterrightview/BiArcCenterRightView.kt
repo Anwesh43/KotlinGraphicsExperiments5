891f2b2a1a812db325bff7f43b5be25f38100e5a
Anwesh43 <anweshthecool0@gmail.com>
2024-12-06 05:59:43 +0530

    biarccenterrightview: adding Canvas extension methods

--- biarccenterrightview/src/main/java/com/example/biarccenterrightview/BiArcCenterRightView.kt
+++ biarccenterrightview/src/main/java/com/example/biarccenterrightview/BiArcCenterRightView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -27,3 +27,36 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiArcCenterRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(0f, (h / 2 - size / 2) * (1 - dsc(2 + j))) {
+                    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), rot / 2, rot * dsc(j), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBACRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiArcCenterRight(scale, w, h, paint)
+}
