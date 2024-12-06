de1402f4fc3d190c701a3ffc8e52b934a249b7de
Anwesh43 <anweshthecool0@gmail.com>
2024-12-06 06:01:58 +0530

    biarccenterrightview: adding BiArcCenterRightView class

--- biarccenterrightview/src/main/java/com/example/biarccenterrightview/BiArcCenterRightView.kt
+++ biarccenterrightview/src/main/java/com/example/biarccenterrightview/BiArcCenterRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBACRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiArcCenterRight(scale, w, h, paint)
 }
+
+class BiArcCenterRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
