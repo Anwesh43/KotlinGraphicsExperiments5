94de4ad381261fde30f4f2eb74e5a6f25a514486
Anwesh43 <anweshthecool0@gmail.com>
2024-12-08 12:53:15 +0530

    linedividebentdownview: adding LDBDNode class

--- linedividebentdownview/src/main/java/com/example/linedividebentdownview/LineDivideBentDownView.kt
+++ linedividebentdownview/src/main/java/com/example/linedividebentdownview/LineDivideBentDownView.kt
@@ -121,4 +121,45 @@ class LineDivideBentDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDBDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDBDNode? = null
+        private var prev : LDBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDBDNode {
+            var curr : LDBDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
