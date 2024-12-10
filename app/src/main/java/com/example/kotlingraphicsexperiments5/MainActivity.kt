3cba64a37d858d8450aca149858f22782ef0a679
Anwesh43 <anweshthecool0@gmail.com>
2024-12-10 11:10:22 +0530

    linedividebentdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biarccenterrightview.BiArcCenterRightView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linedividebentdownview.LineDivideBentDownView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BiArcCenterRightView.create(this)
+        LineDivideBentDownView.create(this)
     }
 }
