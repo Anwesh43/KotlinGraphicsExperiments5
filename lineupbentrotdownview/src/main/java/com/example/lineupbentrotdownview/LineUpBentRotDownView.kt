f5e8ceb327617cf1eb04ab596ee1c80a05dae56c
Anwesh43 <anweshthecool0@gmail.com>
2024-12-11 20:27:07 +0530

    linedividebentdownview: adding LineUpBentRotDownView class

--- lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
+++ lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLUBRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineUpBentRotDown(scale, w, h, paint)
 }
+
+class LineUpBentRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
