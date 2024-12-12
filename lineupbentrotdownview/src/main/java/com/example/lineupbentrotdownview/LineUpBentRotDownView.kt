fa6f87132425159e45cae3cd92aa531c03a5abd2
Anwesh43 <anweshthecool0@gmail.com>
2024-12-12 11:48:32 +0530

    linedividebentdownview: adding renderer in view

--- lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
+++ lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
@@ -62,14 +62,16 @@ fun Canvas.drawLUBRDNode(i : Int, scale : Float, paint : Paint) {
 
 class LineUpBentRotDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -190,8 +192,9 @@ class LineUpBentRotDownView(ctx : Context) : View(ctx) {
     data class Renderer(var view : LineUpBentRotDownView, private val animator : Animator = Animator(view)) {
 
         private val lubrd : LineUpBentRotDown = LineUpBentRotDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             lubrd.draw(canvas, paint)
             animator.animate {
