62b93ac80faec83f48547fd9e1c35a6b7ad0e339
Anwesh43 <anweshthecool0@gmail.com>
2024-12-12 11:43:15 +0530

    linedividebentdownview: adding LUBRDNode class

--- lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
+++ lineupbentrotdownview/src/main/java/com/example/lineupbentrotdownview/LineUpBentRotDownView.kt
@@ -122,4 +122,45 @@ class LineUpBentRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LUBRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LUBRDNode? = null
+        private var prev : LUBRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LUBRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLUBRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LUBRDNode {
+            var curr : LUBRDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
