273fe3774cf44258e76283be414812e264c58f64
Anwesh43 <anweshthecool0@gmail.com>
2024-12-14 10:40:15 +0530

    linebentsiderightview: adding Canvas extension methods

--- linebentsiderightview/src/main/java/com/example/linebentsiderightview/LineBentSideRightView.kt
+++ linebentsiderightview/src/main/java/com/example/linebentsiderightview/LineBentSideRightView.kt
@@ -26,3 +26,32 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentSideRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 -w * 0.5f * dsc(3), h / 2 + (h / 2) * dsc(2)) {
+        drawXY(0f, -h / 2)  {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        }
+    }
+}
+
+fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBentSideRight(scale, w, h, paint)
+}
