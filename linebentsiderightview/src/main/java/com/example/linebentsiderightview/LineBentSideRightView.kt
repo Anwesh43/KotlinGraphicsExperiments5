2a26f7199130ad77680d3547b83cd4600cd78728
Anwesh43 <anweshthecool0@gmail.com>
2024-12-14 10:42:23 +0530

    linebentsiderightview: adding LineBentSideRightView class

--- linebentsiderightview/src/main/java/com/example/linebentsiderightview/LineBentSideRightView.kt
+++ linebentsiderightview/src/main/java/com/example/linebentsiderightview/LineBentSideRightView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentSideRight(scale, w, h, paint)
 }
+
+class LineBentSideRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
