0f74c8d0a4fbba34f99a681816b8bbe71735233a
Anwesh43 <anweshthecool0@gmail.com>
2024-12-15 10:36:02 +0530

    linearcbentjoinview: adding LABJNode class

--- linearcbentjoinview/src/main/java/com/example/linearcbentjoinview/LineArcBentJoinView.kt
+++ linearcbentjoinview/src/main/java/com/example/linearcbentjoinview/LineArcBentJoinView.kt
@@ -123,4 +123,45 @@ class LineArcBentJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LABJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LABJNode? = null
+        private var prev : LABJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LABJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLABJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LABJNode {
+            var curr : LABJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
