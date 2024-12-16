ded28f4b5a63e0ccb26d0bc0574dd7e8b5ed1196
Anwesh43 <anweshthecool0@gmail.com>
2024-12-16 09:36:23 +0530

    rightlinejoinshiftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linearcbentjoinview.LineArcBentJoinView
+import com.example.rightlinejoinshiftview.RightLineJoinShiftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcBentJoinView.create(this)
+        RightLineJoinShiftView.create(this)
     }
 }
