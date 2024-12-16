5011c43aa0e010f52904c038e180b731326a597e
Anwesh43 <anweshthecool0@gmail.com>
2024-12-16 09:11:59 +0530

    rightlinejoinshiftview: adding Canvas extension methods

--- rightlinejoinshiftview/src/main/java/com/example/rightlinejoinshiftview/RightLineJoinShiftView.kt
+++ rightlinejoinshiftview/src/main/java/com/example/rightlinejoinshiftview/RightLineJoinShiftView.kt
@@ -28,3 +28,36 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineJoinShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * (1 - dsc(2)))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(deg * (1 - dsc(2)))
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawRLJSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineJoinShift(scale, w, h, paint)
+}
