2e73c98ea7895427f35ae2812fb0b8492be40730
Anwesh43 <anweshthecool0@gmail.com>
2024-12-16 09:13:17 +0530

    rightlinejoinshiftview: adding RightLineJoinShiftView class

--- rightlinejoinshiftview/src/main/java/com/example/rightlinejoinshiftview/RightLineJoinShiftView.kt
+++ rightlinejoinshiftview/src/main/java/com/example/rightlinejoinshiftview/RightLineJoinShiftView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRLJSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightLineJoinShift(scale, w, h, paint)
 }
+
+class RightLineJoinShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
