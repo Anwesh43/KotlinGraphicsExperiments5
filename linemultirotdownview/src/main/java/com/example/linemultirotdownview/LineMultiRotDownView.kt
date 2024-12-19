84384d807a23c57ef71ba75a99693611837eb074
Anwesh43 <anweshthecool0@gmail.com>
2024-12-19 09:04:49 +0530

    linemultirotdownview: adding Renderer class

--- linemultirotdownview/src/main/java/com/example/linemultirotdownview/LineMultiRotDownView.kt
+++ linemultirotdownview/src/main/java/com/example/linemultirotdownview/LineMultiRotDownView.kt
@@ -186,4 +186,27 @@ class LineMultiRotDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineMultiRotDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lmrd : LineMultiRotDown = LineMultiRotDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lmrd.draw(canvas, paint)
+            animator.animate {
+                lmrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lmrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
