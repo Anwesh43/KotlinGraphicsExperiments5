56bd20a84f6099e3797b33ec1c486cb490c198e1
Anwesh43 <anweshthecool0@gmail.com>
2024-12-20 01:27:30 +0530

    linedropsqbentview: adding LineDropSqBentView class

--- linedropsqbentview/src/main/java/com/example/linedropsqbentview/LineDropSqBentView.kt
+++ linedropsqbentview/src/main/java/com/example/linedropsqbentview/LineDropSqBentView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLDSBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineDropSqBent(scale, w, h, paint)
 }
+
+class LineDropSqBentView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
