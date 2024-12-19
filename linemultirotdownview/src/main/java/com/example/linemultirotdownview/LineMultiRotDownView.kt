a7dac5499ced5b6da0678425ddab4f07adc3e64c
Anwesh43 <anweshthecool0@gmail.com>
2024-12-19 08:50:20 +0530

    linemultirotdownview: adding LineMultiRotDownView class

--- linemultirotdownview/src/main/java/com/example/linemultirotdownview/LineMultiRotDownView.kt
+++ linemultirotdownview/src/main/java/com/example/linemultirotdownview/LineMultiRotDownView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLMRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineMultiRotDown(scale, w, h, paint)
 }
+
+class LineMultiRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
