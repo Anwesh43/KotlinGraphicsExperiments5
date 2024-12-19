897ac9b9c1b6233e3d4ac7b8b06b52139f2d9e95
Anwesh43 <anweshthecool0@gmail.com>
2024-12-20 01:32:19 +0530

    linedropsqbentview: adding LDSBNode class

--- linedropsqbentview/src/main/java/com/example/linedropsqbentview/LineDropSqBentView.kt
+++ linedropsqbentview/src/main/java/com/example/linedropsqbentview/LineDropSqBentView.kt
@@ -124,4 +124,45 @@ class LineDropSqBentView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDSBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDSBNode? = null
+        private var prev : LDSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDSBNode {
+            var curr : LDSBNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
