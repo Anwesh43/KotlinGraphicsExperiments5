530b5fbca3d0c04874197a9404b0580ae5b83fa3
Anwesh43 <anweshthecool0@gmail.com>
2024-12-21 10:58:12 +0530

    linegapextendrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linedropsqbentview.LineDropSqBentView
+import com.example.linegapextendrightview.LineGapExtendRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineDropSqBentView.create(this)
+        LineGapExtendRightView.create(this)
     }
 }
