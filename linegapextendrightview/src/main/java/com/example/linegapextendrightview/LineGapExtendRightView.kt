7a0b37849185dd6e143fbcd1f42ebb032740a362
Anwesh43 <anweshthecool0@gmail.com>
2024-12-21 10:44:17 +0530

    linegapextendrightview: adding LineGapExtendRightView class

--- linegapextendrightview/src/main/java/com/example/linegapextendrightview/LineGapExtendRightView.kt
+++ linegapextendrightview/src/main/java/com/example/linegapextendrightview/LineGapExtendRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLGERNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineGapExtendRight(scale, w, h, paint)
 }
+
+class LineGapExtendRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
