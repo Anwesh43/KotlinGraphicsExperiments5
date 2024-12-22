061cedd6113cc39afa9912aef4f7260f38c0a0d7
Anwesh43 <anweshthecool0@gmail.com>
2024-12-22 11:53:38 +0530

    bentperplineleftview: adding fix

--- bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
+++ bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
@@ -21,7 +21,7 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = -45f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -42,7 +42,7 @@ fun Canvas.drawBentPerpLineLeft(scale : Float, w : Float, h : Float, paint : Pai
     drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
         for (j in 0..1) {
             drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
-                rotate(rot * (1f - 2 * j) * dsc(j + 1))
+                rotate(rot * (1f - 2 * j) * (1 + j) * dsc(j + 1))
                 drawLine(0f, 0f, -size, 0f, paint)
             }
         }
