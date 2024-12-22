fd0bfbed039581f37717adbc97a230471d53f2ae
Anwesh43 <anweshthecool0@gmail.com>
2024-12-22 11:50:07 +0530

    bentperplineleftview: adding static create method

--- bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
+++ bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
@@ -209,4 +209,13 @@ class BentPerpLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : BentPerpLineLeftView {
+            val view : BentPerpLineLeftView = BentPerpLineLeftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
