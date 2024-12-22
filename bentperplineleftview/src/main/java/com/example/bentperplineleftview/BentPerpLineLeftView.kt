a796db079fad8d5694b96f4af2f3f79013f68969
Anwesh43 <anweshthecool0@gmail.com>
2024-12-22 11:39:10 +0530

    bentperplineleftview: adding BPLLNode class

--- bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
+++ bentperplineleftview/src/main/java/com/example/bentperplineleftview/BentPerpLineLeftView.kt
@@ -120,4 +120,45 @@ class BentPerpLineLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BPLLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BPLLNode? = null
+        private var prev : BPLLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BPLLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBPLLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BPLLNode {
+            var curr : BPLLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
