4159a502ca2d6a4dc592f3e54186e73c6faea8db
Anwesh43 <anweshthecool0@gmail.com>
2024-12-23 02:35:57 +0530

    fourquartercircleview: adding FourQuarterCircleView class

--- fourquartercircleview/src/main/java/com/example/fourquartercircleview/FourQuarterCircleView.kt
+++ fourquartercircleview/src/main/java/com/example/fourquartercircleview/FourQuarterCircleView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawFQCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawFourQuarterCircle(scale, w, h, paint)
 }
+
+class FourQuarterCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
