2a81fe950c0cf452ebceffc4f970946a08c1261c
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 02:14:33 +0530

    linerectarcleftview: adding LineRectArcLeftView class

--- linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
+++ linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLRALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRectArcLeft(scale, w, h, paint)
 }
+
+class LineRectArcLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
