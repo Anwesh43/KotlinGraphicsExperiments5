f4897914c7f0f828b22ff586bcda1deb50e3b444
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 02:12:57 +0530

    linerectarcleftview: adding Canvas extension methods

--- linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
+++ linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
@@ -19,10 +19,44 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.9f
+val sizeFactor : Float = 8.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRectArcLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, -size / 2) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawXY(-size, -size / 2) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+        }
+        drawXY(-size, 0f) {
+            drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRALNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRectArcLeft(scale, w, h, paint)
+}
