082e9376523962bca2ac12403026d1e68f038363
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 02:24:27 +0530

    linerectarcleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.fourquartercircleview.FourQuarterCircleView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linerectarcleftview.LineRectArcLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FourQuarterCircleView.create(this)
+        LineRectArcLeftView.create(this)
     }
 }
