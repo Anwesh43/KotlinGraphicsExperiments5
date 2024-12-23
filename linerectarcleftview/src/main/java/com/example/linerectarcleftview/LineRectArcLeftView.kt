c4f9335f6560f4118d07cc854eaf87b4336eda66
Anwesh43 <anweshthecool0@gmail.com>
2024-12-24 02:20:49 +0530

    linerectarcleftview: adding LineRectArcLeft class

--- linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
+++ linerectarcleftview/src/main/java/com/example/linerectarcleftview/LineRectArcLeftView.kt
@@ -164,4 +164,27 @@ class LineRectArcLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRectArcLeft(var i : Int) {
+
+        private var curr : LRALNode = LRALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
