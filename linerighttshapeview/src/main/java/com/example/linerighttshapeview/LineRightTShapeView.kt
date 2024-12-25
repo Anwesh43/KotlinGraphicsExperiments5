865a4ccf0b8d274a8d84f71a4ed1dfd5c25077d9
Anwesh43 <anweshthecool0@gmail.com>
2024-12-25 09:05:06 +0530

    linerighttshapeview: adding LineRightTShape class

--- linerighttshapeview/src/main/java/com/example/linerighttshapeview/LineRightTShapeView.kt
+++ linerighttshapeview/src/main/java/com/example/linerighttshapeview/LineRightTShapeView.kt
@@ -165,4 +165,27 @@ class LineRightTShapeView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRightTShape(var i : Int) {
+
+        private var curr : LRTSNode = LRTSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
