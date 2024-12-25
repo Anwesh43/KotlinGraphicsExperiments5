98dc3b6e5cdbcec1d4e77428ee02ffd4b9cc288f
Anwesh43 <anweshthecool0@gmail.com>
2024-12-25 09:03:55 +0530

    linerighttshapeview: adding LRTSNode class

--- linerighttshapeview/src/main/java/com/example/linerighttshapeview/LineRightTShapeView.kt
+++ linerighttshapeview/src/main/java/com/example/linerighttshapeview/LineRightTShapeView.kt
@@ -124,4 +124,45 @@ class LineRightTShapeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRTSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRTSNode? = null
+        private var prev : LRTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRTSNode {
+            var curr : LRTSNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
