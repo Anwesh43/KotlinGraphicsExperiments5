445644ee75f495dbb6aa3d13430fa94ff897f921
Anwesh43 <anweshthecool0@gmail.com>
2024-12-28 10:00:22 +0530

    eitherlinerotintoview: adding EitherLineRotIntoView class

--- eitherlinerotintoview/src/main/java/com/example/eitherlinerotintoview/EitherLineRotIntoView.kt
+++ eitherlinerotintoview/src/main/java/com/example/eitherlinerotintoview/EitherLineRotIntoView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawELRINode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawEitherLineRotInto(scale, w, h, paint)
 }
+
+class EitherLineRotIntoView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
