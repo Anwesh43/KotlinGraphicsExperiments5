271ee15abeb22bf6d2b99988c4d3e6891d3605d5
Anwesh43 <anweshthecool0@gmail.com>
2024-12-28 09:59:24 +0530

    eitherlinerotintoview: adding Canvas extension methods

--- eitherlinerotintoview/src/main/java/com/example/eitherlinerotintoview/EitherLineRotIntoView.kt
+++ eitherlinerotintoview/src/main/java/com/example/eitherlinerotintoview/EitherLineRotIntoView.kt
@@ -27,3 +27,43 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawEitherLineRotInto(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY((w / 2) * ((1 - dsc(0).divideScale(j, 2)) + dsc(3).divideScale(j, 2)), 0f) {
+                    for (k in 0..1) {
+                        drawXY(0f, 0f) {
+                            rotate(rot * dsc(1).divideScale(j, 2) * k)
+                            drawLine(0f, 0f, size, 0f, paint)
+                        }
+                    }
+                    drawArc(RectF(-size, -size, size, size), 0f, rot * dsc(2).divideScale(j, 2), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawELRINode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawEitherLineRotInto(scale, w, h, paint)
+}
