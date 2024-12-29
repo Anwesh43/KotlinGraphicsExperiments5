c06f62718d354b715aadee4e6be8c6861460367a
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 08:20:08 +0530

    biarcfromupview: adding BAFUNode class

--- biarcfromupview/src/main/java/com/example/biarcfromupview/BiArcFromUpView.kt
+++ biarcfromupview/src/main/java/com/example/biarcfromupview/BiArcFromUpView.kt
@@ -121,4 +121,45 @@ class BiArcFromUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAFUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BAFUNode? = null
+        private var prev : BAFUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BAFUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAFUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAFUNode {
+            var curr : BAFUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
