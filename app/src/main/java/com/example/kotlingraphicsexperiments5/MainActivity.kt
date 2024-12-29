f3d20704980a30a130272791af63b0f42c71bbfb
Anwesh43 <anweshthecool0@gmail.com>
2024-12-29 08:26:22 +0530

    biarcfromupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.eitherlinerotintoview.EitherLineRotIntoView
+import com.example.biarcfromupview.BiArcFromUpView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        EitherLineRotIntoView.create(this)
+        BiArcFromUpView.create(this)
     }
 }
