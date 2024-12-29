313221ca95d08d3e8e7dd234e2446de95ef16f17
Anwesh43 <anweshthecool0@gmail.com>
2024-12-30 01:41:34 +0530

    sqexpandrightupview: adding SqExpandRightUpView class

--- sqexpandrightupview/src/main/java/com/example/sqexpandrightupview/SqExpandRightUpView.kt
+++ sqexpandrightupview/src/main/java/com/example/sqexpandrightupview/SqExpandRightUpView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawSERUNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSqExpandRightUp(scale, w, h, paint)
 }
+
+class SqExpandRightUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
