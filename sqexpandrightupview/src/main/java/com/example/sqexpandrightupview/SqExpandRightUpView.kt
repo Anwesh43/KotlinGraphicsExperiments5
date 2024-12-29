b81f1b4ef6b895486dba69e72a5cacca33f3b499
Anwesh43 <anweshthecool0@gmail.com>
2024-12-30 01:36:07 +0530

    sqexpandrightupview: adding Canvas extension methods

--- sqexpandrightupview/src/main/java/com/example/sqexpandrightupview/SqExpandRightUpView.kt
+++ sqexpandrightupview/src/main/java/com/example/sqexpandrightupview/SqExpandRightUpView.kt
@@ -26,3 +26,28 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqExpandRightUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * dsc(2), h / 2 + (h / 2 - size / 2) * dsc(1)) {
+        rotate(rot * dsc(3))
+        drawRect(RectF(0f, -size / 2, size * dsc(0), size / 2), paint)
+    }
+}
+
+fun Canvas.drawSERUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawSqExpandRightUp(scale, w, h, paint)
+}
