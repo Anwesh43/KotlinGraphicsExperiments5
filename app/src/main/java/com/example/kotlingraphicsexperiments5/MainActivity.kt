22526c95969ae2162c39bce37ddd099919b89a1d
Anwesh43 <anweshthecool0@gmail.com>
2024-12-31 02:46:17 +0530

    sqmiduprotview: adding view in activit

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,11 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sqexpandrightupview.SqExpandRightUpView
+import com.example.sqmiduprotview.SqMidRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SqExpandRightUpView.create(this)
+        SqMidRotUpView.create(this)
     }
 }
