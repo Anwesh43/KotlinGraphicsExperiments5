19faaa15f4a8f2696198df324f0e39e6d5b6a867
Anwesh43 <anweshthecool0@gmail.com>
2024-12-31 02:27:13 +0530

    sqmiduprotview: adding constants and librarie

---
+++ sqmiduprotview/src/main/java/com/example/sqmiduprotview/SqMidRotUpView.kt
@@ -0,0 +1,25 @@
+package com.example.sqmiduprotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val rot : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
