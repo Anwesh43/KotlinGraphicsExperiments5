f0b7b9b59e9231282c9046e2cc9a17243ed91693
Anwesh43 <anweshthecool0@gmail.com>
2025-01-01 01:09:55 +0530

    eitherlinebentrotview: adding ELBRNode class

--- eitherlinebentrotview/src/main/java/com/example/eitherlinebentrotview/EitherLineBentRotView.kt
+++ eitherlinebentrotview/src/main/java/com/example/eitherlinebentrotview/EitherLineBentRotView.kt
@@ -121,4 +121,45 @@ class EitherLineBentRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ELBRNode? = null
+        private var prev : ELBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ELBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELBRNode {
+            var curr : ELBRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
