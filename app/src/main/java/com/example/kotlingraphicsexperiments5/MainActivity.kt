f98af5687e48049ee3318fe40bb2f3f9866255d3
Anwesh43 <anweshthecool0@gmail.com>
2025-01-01 01:15:56 +0530

    eitherlinebentrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.eitherlinebentrotview.EitherLineBentRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sqmiduprotview.SqMidRotUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SqMidRotUpView.create(this)
+        EitherLineBentRotView.create(this)
     }
 }
