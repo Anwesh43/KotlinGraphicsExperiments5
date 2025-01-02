585bb99792058daea06cf5854aad8ff780c510ca
Anwesh43 <anweshthecool0@gmail.com>
2025-01-02 15:32:12 +0530

    linefromhalfupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.eitherlinebentrotview.EitherLineBentRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linefromhalfupview.LineFromHalfUpView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        EitherLineBentRotView.create(this)
+        LineFromHalfUpView.create(this)
     }
 }
