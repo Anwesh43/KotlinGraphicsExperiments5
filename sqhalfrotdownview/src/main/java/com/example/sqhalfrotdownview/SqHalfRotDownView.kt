3e2d14f23e57eca8e11fcfc6965163cc82d4309f
Anwesh43 <anweshthecool0@gmail.com>
2025-01-05 11:15:23 +0530

    sqhalfrotdownview: adding SqHalfRotDown class

--- sqhalfrotdownview/src/main/java/com/example/sqhalfrotdownview/SqHalfRotDownView.kt
+++ sqhalfrotdownview/src/main/java/com/example/sqhalfrotdownview/SqHalfRotDownView.kt
@@ -155,4 +155,27 @@ class SqHalfRotDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqHalfRotDown(var i : Int) {
+
+        private var curr : SHRDNode = SHRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
