a2b2ece6f2feb239726c9313f82d51caa566aaff
Anwesh43 <anweshthecool0@gmail.com>
2025-01-05 11:52:08 +0530

    sqhalfrotdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dropbentrightlineview.DropBentRightLineView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.sqhalfrotdownview.SqHalfRotDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DropBentRightLineView.create(this)
+        SqHalfRotDownView.create(this)
     }
 }
