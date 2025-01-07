f0211a158a884e75ddfe297bfb92e5ccd70c9575
Anwesh43 <anweshthecool0@gmail.com>
2025-01-07 12:04:27 +0530

    multirotdownlineview: adding Canvas extension methods

--- multirotdownlineview/src/main/java/com/example/multirotdownlineview/MultiRotDownLineView.kt
+++ multirotdownlineview/src/main/java/com/example/multirotdownlineview/MultiRotDownLineView.kt
@@ -21,8 +21,36 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot1 : Float = 270f
+val rot2 : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMultiRotDownLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(2)) {
+        rotate(rot1 * dsc(1) + rot2 * dsc(3))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+    }
+}
+
+fun Canvas.drawMRDLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMultiRotDownLine(scale, w, h, paint)
+}
