2348c8d4e665e09c20749e9a1acccc6f551ffd7e
Anwesh43 <anweshthecool0@gmail.com>
2025-01-08 11:00:42 +0530

    linebisectsteprightview: adding Canvas extension methods

--- linebisectsteprightview/src/main/java/com/example/linebisectsteprightview/LineBisectStepRightView.kt
+++ linebisectsteprightview/src/main/java/com/example/linebisectsteprightview/LineBisectStepRightView.kt
@@ -27,3 +27,36 @@ val sizeFactor : Float = 5.7f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBisectStepRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(deg * dsc(3))
+        for (j in 0..1) {
+            drawXY(-w / 2 + (w / 2) * dsc(2).divideScale(j, 2), 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(1).divideScale(j, 2))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBisectStepRight(scale, w, h, paint)
+}
+
