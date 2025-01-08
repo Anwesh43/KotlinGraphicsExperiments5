eb21f8fbb9305aa2b1ae95d154b1a35a1f626a30
Anwesh43 <anweshthecool0@gmail.com>
2025-01-08 11:16:14 +0530

    linebisectsteprightview: adding LineBisectStepRightView class

--- linebisectsteprightview/src/main/java/com/example/linebisectsteprightview/LineBisectStepRightView.kt
+++ linebisectsteprightview/src/main/java/com/example/linebisectsteprightview/LineBisectStepRightView.kt
@@ -60,3 +60,18 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     drawLineBisectStepRight(scale, w, h, paint)
 }
 
+class LineBisectStepRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
