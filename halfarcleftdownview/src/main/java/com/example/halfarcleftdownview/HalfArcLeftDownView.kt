1171ce9e9592ce68e60cfc7e703f3b2d5f92c02e
Anwesh43 <anweshthecool0@gmail.com>
2025-01-09 12:35:51 +0530

    halfarcleftdownview: adding Renderer class

--- halfarcleftdownview/src/main/java/com/example/halfarcleftdownview/HalfArcLeftDownView.kt
+++ halfarcleftdownview/src/main/java/com/example/halfarcleftdownview/HalfArcLeftDownView.kt
@@ -182,4 +182,27 @@ class HalfArcLeftDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfArcLeftDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val hald : HalfArcLeftDown = HalfArcLeftDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hald.draw(canvas, paint)
+            animator.animate {
+                hald.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hald.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
