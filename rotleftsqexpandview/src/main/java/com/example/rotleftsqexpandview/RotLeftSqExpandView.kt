adb7407c44a9962386c6a7735a6d2cb122cf45d3
Anwesh43 <anweshthecool0@gmail.com>
2025-01-13 09:51:11 +0530

    rotleftsqexpandview: adding RotLeftSqExpandView class

--- rotleftsqexpandview/src/main/java/com/example/rotleftsqexpandview/RotLeftSqExpandView.kt
+++ rotleftsqexpandview/src/main/java/com/example/rotleftsqexpandview/RotLeftSqExpandView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawRLSENode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawRotLeftSqExpand(scale, w, h, paint)
 }
+
+class RotLeftSqExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
