c03e26b24043e04de73b4cecba69d714f499661d
Anwesh43 <anweshthecool0@gmail.com>
2025-01-13 09:44:31 +0530

    rotleftsqexpandview: adding constants and libraries

---
+++ rotleftsqexpandview/src/main/java/com/example/rotleftsqexpandview/RotLeftSqExpandView.kt
@@ -0,0 +1,24 @@
+package com.example.rotleftsqexpandview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 5.8f
+val rot : Float = 180f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
