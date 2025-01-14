4ca228984a09675755acad83c42ebfdaa2157dcb
Anwesh43 <anweshthecool0@gmail.com>
2025-01-15 01:11:41 +0530

    linerothalfdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sqfromcornerdownview.SqFromCornerDownView
+import com.example.linerothalfdownview.LineRotHalfDownView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SqFromCornerDownView.create(this)
+        LineRotHalfDownView.create(this)
     }
 }
