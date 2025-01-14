3b14968af09261858be572d6687da685df4e6d98
Anwesh43 <anweshthecool0@gmail.com>
2025-01-15 01:00:17 +0530

    linerothalfdownview: adding LRHDNode class

--- linerothalfdownview/src/main/java/com/example/linerothalfdownview/LineRotHalfDownView.kt
+++ linerothalfdownview/src/main/java/com/example/linerothalfdownview/LineRotHalfDownView.kt
@@ -120,4 +120,45 @@ class LineRotHalfDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRHDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRHDNode? = null
+        private var prev : LRHDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRHDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRHDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRHDNode {
+            var curr : LRHDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
