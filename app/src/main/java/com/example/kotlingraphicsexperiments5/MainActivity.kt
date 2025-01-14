adb1c4b3fa07fde025e9e176ce29a9986f0f86ad
Anwesh43 <anweshthecool0@gmail.com>
2025-01-14 12:41:38 +0530

    sqfromcornerdownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.rotleftsqexpandview.RotLeftSqExpandView
+import com.example.sqfromcornerdownview.SqFromCornerDownView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotLeftSqExpandView.create(this)
+        SqFromCornerDownView.create(this)
     }
 }
