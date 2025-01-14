0f9e73acc337739f672be0912f92285bd81bff91
Anwesh43 <anweshthecool0@gmail.com>
2025-01-14 12:06:11 +0530

    sqfromcornerdownview: adding SFCDNode class

--- sqfromcornerdownview/src/main/java/com/example/sqfromcornerdownview/SqFromCornerDownView.kt
+++ sqfromcornerdownview/src/main/java/com/example/sqfromcornerdownview/SqFromCornerDownView.kt
@@ -114,4 +114,45 @@ class SqFromCornerDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SFCDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SFCDNode? = null
+        private var prev : SFCDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SFCDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSFCDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SFCDNode {
+            var curr : SFCDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
