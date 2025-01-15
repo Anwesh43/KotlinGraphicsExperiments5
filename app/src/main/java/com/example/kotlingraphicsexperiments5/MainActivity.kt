7e4a6131a6e59bbc1e694095cbcea6280273cf42
Anwesh43 <anweshthecool0@gmail.com>
2025-01-16 01:02:18 +0530

    arcrightrotvanishview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcrightrotvanishview.ArcRightRotVanishView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linerothalfdownview.LineRotHalfDownView
+
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotHalfDownView.create(this)
+        ArcRightRotVanishView.create(this)
     }
 }
