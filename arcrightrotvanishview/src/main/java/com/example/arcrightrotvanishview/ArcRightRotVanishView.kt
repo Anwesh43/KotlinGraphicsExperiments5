5003450e0aac11634570f3de2315b38971fc8257
Anwesh43 <anweshthecool0@gmail.com>
2025-01-16 00:37:57 +0530

    arcrightrotvanishview: adding Canvas extension methods

--- arcrightrotvanishview/src/main/java/com/example/arcrightrotvanishview/ArcRightRotVanishView.kt
+++ arcrightrotvanishview/src/main/java/com/example/arcrightrotvanishview/ArcRightRotVanishView.kt
@@ -22,7 +22,36 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcRightRotVanish(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * dsc(1), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f + 180f * dsc(3), 180f * (dsc(0) - dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawARRVNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcRightRotVanish(scale, w, h, paint)
+}
