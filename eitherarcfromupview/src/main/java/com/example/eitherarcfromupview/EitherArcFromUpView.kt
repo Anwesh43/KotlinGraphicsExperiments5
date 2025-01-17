61006c264a059ae6a38db18541bfcab8eec37f32
Anwesh43 <anweshthecool0@gmail.com>
2025-01-17 16:26:13 +0530

    eitherarcfromupview: adding EitherArcFromUpView class

--- eitherarcfromupview/src/main/java/com/example/eitherarcfromupview/EitherArcFromUpView.kt
+++ eitherarcfromupview/src/main/java/com/example/eitherarcfromupview/EitherArcFromUpView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawEAFUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawEitherArcFromUp(scale, w, h, paint)
 }
+
+class EitherArcFromUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
