54a334aebc9bedfe780f8a674bc38f6030e72e8c
Anwesh43 <anweshthecool0@gmail.com>
2025-01-17 16:37:18 +0530

    eitherarcfromupview: adding EitherArcFromUp class

--- eitherarcfromupview/src/main/java/com/example/eitherarcfromupview/EitherArcFromUpView.kt
+++ eitherarcfromupview/src/main/java/com/example/eitherarcfromupview/EitherArcFromUpView.kt
@@ -171,4 +171,27 @@ class EitherArcFromUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class EitherArcFromUp(var i : Int = 0, val state : State = State()) {
+
+        private var curr : EAFUNode = EAFUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
