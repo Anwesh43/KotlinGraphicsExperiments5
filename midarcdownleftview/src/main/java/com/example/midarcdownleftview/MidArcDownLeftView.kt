8a9b200b9e083dddf7ded07eb491be59594539ac
Anwesh43 <anweshthecool0@gmail.com>
2025-01-18 13:47:16 +0530

    midarcdownleftview: adding MADLNode class

--- midarcdownleftview/src/main/java/com/example/midarcdownleftview/MidArcDownLeftView.kt
+++ midarcdownleftview/src/main/java/com/example/midarcdownleftview/MidArcDownLeftView.kt
@@ -123,4 +123,45 @@ class MidArcDownLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MADLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MADLNode? = null
+        private var prev : MADLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MADLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMADLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MADLNode {
+            var curr : MADLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
