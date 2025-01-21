ad70bd1b564572073f4399b32da808d47d4a01bb
Anwesh43 <anweshthecool0@gmail.com>
2025-01-22 01:14:28 +0530

    linebentjoinarcview: adding LBJANode class

--- linebentjoinarcview/src/main/java/com/example/linebentjoinarcview/LineBentJoinArcView.kt
+++ linebentjoinarcview/src/main/java/com/example/linebentjoinarcview/LineBentJoinArcView.kt
@@ -121,4 +121,45 @@ class LineBentJoinArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBJANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBJANode? = null
+        private var prev : LBJANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBJANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBJANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBJANode {
+            var curr : LBJANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
