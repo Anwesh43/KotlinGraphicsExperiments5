d619de6f54ea347434c8a8d61e7a29ab952cc80f
Anwesh43 <anweshthecool0@gmail.com>
2025-01-23 14:48:16 +0530

    sqrotcornerdownview: adding Canvas extension methods

--- sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
+++ sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -26,3 +26,28 @@ val rot : Float = 270f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqCornerRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * (dsc(2) + dsc(5)), (h / 2) * (dsc(1) + dsc(4))) {
+        rotate(rot * dsc(3))
+        drawRect(RectF(0f, 0f, size * dsc(0), size * dsc(0)), paint)
+    }
+}
+
+fun Canvas.drawSCRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawSqCornerRotDown(scale, w, h, paint)
+}
