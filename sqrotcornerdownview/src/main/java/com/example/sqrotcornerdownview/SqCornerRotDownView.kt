c08a3f3c41928eaf641de4d578460a6cf20bd92a
Anwesh43 <anweshthecool0@gmail.com>
2025-01-23 16:18:52 +0530

    sqrotcornerdownview: adding SCRDNode class

--- sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
+++ sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
@@ -113,4 +113,45 @@ class SqCornerRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SCRDNode? = null
+        private var prev : SCRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SCRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCRDNode {
+            var curr : SCRDNode? = prev
+            if (dir === 1) {
+                curr = this.next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
