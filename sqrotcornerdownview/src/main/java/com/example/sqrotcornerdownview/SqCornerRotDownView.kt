79fb4f106a804cdc246788635b98f539f6498d70
Anwesh43 <anweshthecool0@gmail.com>
2025-01-23 14:49:19 +0530

    sqrotcornerdownview: adding SqCornerRotDownView class

--- sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
+++ sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawSCRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSqCornerRotDown(scale, w, h, paint)
 }
+
+class SqCornerRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
