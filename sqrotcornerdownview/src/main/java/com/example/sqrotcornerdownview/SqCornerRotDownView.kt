a0ddf9d3ee4ab8bf6b3adc020e35b9718c632c45
Anwesh43 <anweshthecool0@gmail.com>
2025-01-23 16:14:40 +0530

    sqrotcornerdownview: adding Animator class

--- sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
+++ sqrotcornerdownview/src/main/java/com/example/sqrotcornerdownview/SqCornerRotDownView.kt
@@ -86,4 +86,31 @@ class SqCornerRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                } catch(ex : Exception){
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
