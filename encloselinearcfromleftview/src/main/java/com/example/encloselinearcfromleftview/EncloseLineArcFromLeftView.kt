80635b024227d15d67ea72d18bbeaa3d60334d12
Anwesh43 <anweshthecool0@gmail.com>
2025-01-24 11:57:34 +0530

    encloselinearcfromleftview: adding ELAFLNode class

--- encloselinearcfromleftview/src/main/java/com/example/encloselinearcfromleftview/EncloseLineArcFromLeftView.kt
+++ encloselinearcfromleftview/src/main/java/com/example/encloselinearcfromleftview/EncloseLineArcFromLeftView.kt
@@ -123,4 +123,45 @@ class EncloseLineArcFromLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELAFLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : ELAFLNode? = null
+        private var next : ELAFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ELAFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELAFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELAFLNode {
+            var curr : ELAFLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
