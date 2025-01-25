894498051f2c6d2bcaa8e038499cfdebf9a3325c
Anwesh43 <anweshthecool0@gmail.com>
2025-01-26 01:23:27 +0530

    semiarcrotdownview: adding SARDNode class

--- semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
+++ semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
@@ -117,4 +117,45 @@ class SemiArcRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SARDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SARDNode? = null
+        private var prev : SARDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SARDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSARDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SARDNode {
+            var curr : SARDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
