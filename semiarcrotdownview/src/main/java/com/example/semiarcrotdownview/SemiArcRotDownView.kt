aba4bb6ddb3197b8450b54368407ad4d0087e125
Anwesh43 <anweshthecool0@gmail.com>
2025-01-26 01:30:59 +0530

    semiarcrotdownview: adding fix

--- semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
+++ semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
@@ -52,6 +52,7 @@ fun Canvas.drawSARDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawSemiArcRotDown(scale, w, h, paint)
 }
 
