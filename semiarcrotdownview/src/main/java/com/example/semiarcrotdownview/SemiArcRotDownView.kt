eee89fa6037c749ffa7efcc728cc304029e765b1
Anwesh43 <anweshthecool0@gmail.com>
2025-01-26 01:28:34 +0530

    semiarcrotdownview: adding static create method

--- semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
+++ semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
@@ -206,4 +206,13 @@ class SemiArcRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : SemiArcRotDownView {
+            val view : SemiArcRotDownView = SemiArcRotDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
