c473280e6ab6cf221d85261218982e2d962bff3e
Anwesh43 <anweshthecool0@gmail.com>
2025-01-25 13:17:18 +0530

    arccompleterotupview: adding ACRUNode class

--- arccompleterotupview/src/main/java/com/example/arccompleterotupview/ArcCompleteRotUpView.kt
+++ arccompleterotupview/src/main/java/com/example/arccompleterotupview/ArcCompleteRotUpView.kt
@@ -121,4 +121,45 @@ class ArcCompleteRotUpView(ctx : Context)  : View(ctx) {
             }
         }
     }
+
+    data class ACRUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ACRUNode? = null
+        private var prev : ACRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ACRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACRUNode {
+            var curr : ACRUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
