7760012493d1e79e6d2c8538544ba8537c522df1
Anwesh43 <anweshthecool0@gmail.com>
2025-01-26 01:17:25 +0530

    semiarcrotdownview: adding SemiArcRotDownView class

--- semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
+++ semiarcrotdownview/src/main/java/com/example/semiarcrotdownview/SemiArcRotDownView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawSARDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSemiArcRotDown(scale, w, h, paint)
 }
+
+class SemiArcRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
