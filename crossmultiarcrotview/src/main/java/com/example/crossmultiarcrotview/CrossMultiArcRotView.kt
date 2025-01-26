d8782db0336794ed6a4331dca8cc4097b630f0d4
Anwesh43 <anweshthecool0@gmail.com>
2025-01-27 00:30:09 +0530

    crossmultiarcrotview: adding constants and libraries

---
+++ crossmultiarcrotview/src/main/java/com/example/crossmultiarcrotview/CrossMultiArcRotView.kt
@@ -0,0 +1,27 @@
+package com.example.crossmultiarcrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 11.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
+val deg : Float = 45f
+
+
+
