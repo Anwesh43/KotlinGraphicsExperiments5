f912ea54e1a67fc47f7ae2051a5605b5a3aad922
Anwesh43 <anweshthecool0@gmail.com>
2025-01-27 00:38:42 +0530

    crossmultiarcrotview: adding CrossMultiArcRotView class

--- crossmultiarcrotview/src/main/java/com/example/crossmultiarcrotview/CrossMultiArcRotView.kt
+++ crossmultiarcrotview/src/main/java/com/example/crossmultiarcrotview/CrossMultiArcRotView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawCMARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawCrossMultiArcRot(scale, w, h, paint)
 }
+
+class CrossMultiArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
