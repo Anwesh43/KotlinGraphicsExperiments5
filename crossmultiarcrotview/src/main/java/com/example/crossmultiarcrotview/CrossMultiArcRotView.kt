542c3752a52e271d5042cdfaae06e8898d5d7f9c
Anwesh43 <anweshthecool0@gmail.com>
2025-01-27 00:37:22 +0530

    crossmultiarcrotview: adding Canvas extension methods

--- crossmultiarcrotview/src/main/java/com/example/crossmultiarcrotview/CrossMultiArcRotView.kt
+++ crossmultiarcrotview/src/main/java/com/example/crossmultiarcrotview/CrossMultiArcRotView.kt
@@ -27,3 +27,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCrossMultiArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * j * dsc(2))
+                for (k in 0..1) {
+                    rotate(rot * k * dsc(1))
+                    drawCircle(0f, 0f, size * (dsc(0) - dsc(3)), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawCMARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawCrossMultiArcRot(scale, w, h, paint)
+}
