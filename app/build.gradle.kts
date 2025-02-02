6ed778e5ce76d286bdc9ab43153a4e63d6999707
Anwesh43 <anweshthecool0@gmail.com>
2025-02-02 17:25:01 +0530

    linecatchrightlineview: adding view in activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":linerightrotdownview"))
+    implementation(project(":linecatchrightlineview"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
