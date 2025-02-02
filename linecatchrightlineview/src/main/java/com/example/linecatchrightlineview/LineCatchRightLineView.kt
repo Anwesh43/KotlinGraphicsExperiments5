9b6d0fbd2fcc56c07ca8368242f028a94e9fdf54
Anwesh43 <anweshthecool0@gmail.com>
2025-02-02 17:12:31 +0530

    linecatchrightlineview: adding Stage class

--- linecatchrightlineview/src/main/java/com/example/linecatchrightlineview/LineCatchRightLineView.kt
+++ linecatchrightlineview/src/main/java/com/example/linecatchrightlineview/LineCatchRightLineView.kt
@@ -59,3 +59,18 @@ fun Canvas.drawLCRLNode(i : Int, scale : Float, paint : Paint) {
     drawLineCatchRightLine(scale, w, h, paint)
 }
 
+class LineCatchRightLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
