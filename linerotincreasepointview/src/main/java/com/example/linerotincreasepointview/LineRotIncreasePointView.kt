ca7cc4d3a0f6e7fe926d94fcac421271d2e1a54d
Anwesh43 <anweshthecool0@gmail.com>
2025-02-05 13:10:57 +0530

    linerotincreasepointview: adding LineRotIncreasePointView class

--- linerotincreasepointview/src/main/java/com/example/linerotincreasepointview/LineRotIncreasePointView.kt
+++ linerotincreasepointview/src/main/java/com/example/linerotincreasepointview/LineRotIncreasePointView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLRIPNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotIncreasePoint(scale, w, h, paint)
 }
+
+class LineRotIncreasePointView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
