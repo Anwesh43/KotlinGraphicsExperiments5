d4a08ef632220c47235cf4a4bcafe8c2d87a8b79
Anwesh43 <anweshthecool0@gmail.com>
2025-02-06 13:00:15 +0530

    arcsidebentlineview: adding ArcSideBentLineView class

--- arcsidebentlineview/src/main/java/com/example/arcsidebentlineview/ArcSideBentLineView.kt
+++ arcsidebentlineview/src/main/java/com/example/arcsidebentlineview/ArcSideBentLineView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawASBLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcSideBentLine(scale, w, h, paint)
 }
+
+class ArcSideBentLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
