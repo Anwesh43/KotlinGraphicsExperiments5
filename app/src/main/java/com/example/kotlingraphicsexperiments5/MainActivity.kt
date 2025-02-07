d7b6335dc12410556ac3629f6b1a825cbae6b331
Anwesh43 <anweshthecool0@gmail.com>
2025-02-07 23:09:25 +0530

    bilineturnupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcsidebentlineview.ArcSideBentLineView
+import com.example.bilineturnupview.BiLineTurnUpView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcSideBentLineView.create(this)
+        BiLineTurnUpView.create(this)
     }
 }
