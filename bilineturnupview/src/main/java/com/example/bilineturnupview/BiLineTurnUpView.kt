b2d29b56f29315b2fc0f3d1fca40630cf479d7c0
Anwesh43 <anweshthecool0@gmail.com>
2025-02-07 12:47:42 +0530

    bilineturnupview: adding BiLineTurnUpView class

--- bilineturnupview/src/main/java/com/example/bilineturnupview/BiLineTurnUpView.kt
+++ bilineturnupview/src/main/java/com/example/bilineturnupview/BiLineTurnUpView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawBLTUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineTurnUp(scale, w, h, paint)
 }
+
+class BiLineTurnUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
