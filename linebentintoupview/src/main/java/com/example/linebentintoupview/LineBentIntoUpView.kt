6afa10b88cc8a4fc7b94ba5b093283efa9013662
Anwesh43 <anweshthecool0@gmail.com>
2025-02-10 16:15:54 +0530

    linebentintoupview: adding LineBentIntoUpView class

--- linebentintoupview/src/main/java/com/example/linebentintoupview/LineBentIntoUpView.kt
+++ linebentintoupview/src/main/java/com/example/linebentintoupview/LineBentIntoUpView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLBIUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBentIntoUp(scale, w, h, paint)
 }
+
+class LineBentIntoUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
