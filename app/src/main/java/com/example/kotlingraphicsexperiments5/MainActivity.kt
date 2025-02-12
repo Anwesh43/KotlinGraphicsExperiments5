cf0553dd4a08042faab66967b42c24eb91b27366
Anwesh43 <anweshthecool0@gmail.com>
2025-02-12 14:30:36 +0530

    linerotarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebentintoupview.LineBentIntoUpView
+import com.example.linerotarcrightview.LineRotArcRightView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBentIntoUpView.create(this)
+        LineRotArcRightView.create(this)
     }
 }
