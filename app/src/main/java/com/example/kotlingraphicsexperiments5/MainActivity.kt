d9d2e7d93d70a93c7973aff517e88822c9f5d511
Anwesh43 <anweshthecool0@gmail.com>
2025-02-13 01:45:03 +0530

    arcpartleftrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcpartleftrightview.ArcPartLeftRightView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linerotarcrightview.LineRotArcRightView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineRotArcRightView.create(this)
+        ArcPartLeftRightView.create(this)
     }
 }
