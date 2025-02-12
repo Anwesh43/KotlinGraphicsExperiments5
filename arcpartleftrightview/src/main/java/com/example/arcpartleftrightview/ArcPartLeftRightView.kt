eee8d460e1718be022f2e31dd28baf9b52455dc2
Anwesh43 <anweshthecool0@gmail.com>
2025-02-13 01:13:09 +0530

    arcpartleftrightview: adding ArcPartLeftRightView class

--- arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
+++ arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawAPLRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcPartLeftRight(scale, w, h, paint)
 }
+
+class ArcPartLeftRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
