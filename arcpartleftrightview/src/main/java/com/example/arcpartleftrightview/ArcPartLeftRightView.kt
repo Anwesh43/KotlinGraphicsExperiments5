99d1dd4e7609a6b7a5f3b0751ab66ca0a52f71fc
Anwesh43 <anweshthecool0@gmail.com>
2025-02-13 01:07:44 +0530

    arcpartleftrightview: adding Canvas extension methods

--- arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
+++ arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
@@ -16,7 +16,7 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -28,3 +28,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcPartLeftRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * (dsc(1) + dsc(4)), h / 2) {
+        rotate(rot * dsc(3))
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 90f * (dsc(0) + dsc(2)), false, paint)
+    }
+}
+
+fun Canvas.drawAPLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcPartLeftRight(scale, w, h, paint)
+}
