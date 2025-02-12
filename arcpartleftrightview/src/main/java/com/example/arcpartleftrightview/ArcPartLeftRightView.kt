5ca2aced1ed5b5a475621b0e5109dc561d411310
Anwesh43 <anweshthecool0@gmail.com>
2025-02-13 01:28:14 +0530

    arcpartleftrightview: adding ArcPartLeftRight class

--- arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
+++ arcpartleftrightview/src/main/java/com/example/arcpartleftrightview/ArcPartLeftRightView.kt
@@ -118,4 +118,45 @@ class ArcPartLeftRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class APLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : APLRNode? = null
+        private var prev : APLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = APLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAPLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : APLRNode {
+            var curr : APLRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
