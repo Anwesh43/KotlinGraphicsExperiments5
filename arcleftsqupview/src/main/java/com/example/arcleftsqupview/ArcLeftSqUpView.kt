943e4358fc1cb3ba162c898574c936114d092658
Anwesh43 <anweshthecool0@gmail.com>
2025-02-15 14:22:41 +0530

    arcleftsqupview: adding ALSUNode class

--- arcleftsqupview/src/main/java/com/example/arcleftsqupview/ArcLeftSqUpView.kt
+++ arcleftsqupview/src/main/java/com/example/arcleftsqupview/ArcLeftSqUpView.kt
@@ -122,4 +122,45 @@ class ArcLeftSqUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALSUNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : ALSUNode? = null
+        private var next : ALSUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALSUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALSUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALSUNode {
+            var curr : ALSUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
