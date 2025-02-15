37aa108abfc175b6d49034d81a3117b9184fa8c7
Anwesh43 <anweshthecool0@gmail.com>
2025-02-16 00:31:00 +0530

    arclinemovecenterview: adding ArcLineMoveCenterView class

--- arclinemovecenterview/src/main/java/com/example/arclinemovecenterview/ArcLineMoveCenterView.kt
+++ arclinemovecenterview/src/main/java/com/example/arclinemovecenterview/ArcLineMoveCenterView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawALMCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawArcLineMoveCenter(scale, w, h, paint)
 }
+
+class ArcLineMoveCenterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
