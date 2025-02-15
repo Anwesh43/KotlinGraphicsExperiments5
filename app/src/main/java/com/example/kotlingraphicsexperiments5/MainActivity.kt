f29c2cda4001b958434e0991e4b08a38d2f374f7
Anwesh43 <anweshthecool0@gmail.com>
2025-02-15 14:35:20 +0530

    arcleftsqupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcpartleftrightview.ArcPartLeftRightView
+import com.example.arcleftsqupview.ArcLeftSqUpView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcPartLeftRightView.create(this)
+        ArcLeftSqUpView.create(this)
     }
 }
