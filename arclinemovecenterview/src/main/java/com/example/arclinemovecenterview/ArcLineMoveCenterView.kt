4c762a6ee799e579c891c3f84211be9f71e4c606
Anwesh43 <anweshthecool0@gmail.com>
2025-02-16 00:43:23 +0530

    arclinemovecenterview: adding ALMCNode class

--- arclinemovecenterview/src/main/java/com/example/arclinemovecenterview/ArcLineMoveCenterView.kt
+++ arclinemovecenterview/src/main/java/com/example/arclinemovecenterview/ArcLineMoveCenterView.kt
@@ -128,4 +128,45 @@ class ArcLineMoveCenterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALMCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALMCNode? = null
+        private var prev : ALMCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALMCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALMCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALMCNode {
+            var curr : ALMCNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
