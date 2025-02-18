d219f4fa5d4e4e7a6e2278e2e12b4e6c74c35427
Anwesh43 <anweshthecool0@gmail.com>
2025-02-18 11:18:03 +0530

    bentlinerightview: adding BentLineRightView class

--- bentlinerightview/src/main/java/com/example/bentlinerightview/BentLineRightView.kt
+++ bentlinerightview/src/main/java/com/example/bentlinerightview/BentLineRightView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawBLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentLineRight(scale, w, h, paint)
 }
+
+class BentLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
