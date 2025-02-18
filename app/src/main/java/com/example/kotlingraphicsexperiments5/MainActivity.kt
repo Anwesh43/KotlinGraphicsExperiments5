8506cf87de98c7cfcd7d7e6f52ff9eeaa01f66e2
Anwesh43 <anweshthecool0@gmail.com>
2025-02-19 03:38:33 +0530

    lineperpintorotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bentlinerightview.BentLineRightView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.lineperpintorotview.LinePerpIntoRotView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineRightView.create(this)
+        LinePerpIntoRotView.create(this)
     }
 }
