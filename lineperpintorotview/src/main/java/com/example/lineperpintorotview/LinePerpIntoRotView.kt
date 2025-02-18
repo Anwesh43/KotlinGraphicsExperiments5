287afb4e3a9816a4de962f996c14f14141296360
Anwesh43 <anweshthecool0@gmail.com>
2025-02-19 03:33:09 +0530

    lineperpintorotview: adding LinePerpIntoRot class

--- lineperpintorotview/src/main/java/com/example/lineperpintorotview/LinePerpIntoRotView.kt
+++ lineperpintorotview/src/main/java/com/example/lineperpintorotview/LinePerpIntoRotView.kt
@@ -162,4 +162,27 @@ class LinePerpIntoRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinePerpIntoRot(var i : Int) {
+
+        var curr : LPIRNode = LPIRNode(0)
+        var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
