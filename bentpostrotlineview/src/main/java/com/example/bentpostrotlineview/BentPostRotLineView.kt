4a46fe0a2a117d153d08acece2718d0e7a5b88c4
Anwesh43 <anweshthecool0@gmail.com>
2025-02-20 01:42:59 +0530

    bentpostrotlineview: adding Canvas extension methods

--- bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
+++ bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
@@ -15,15 +15,41 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentPostLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3) - w * dsc(4), h / 2) {
+        rotate(rot * (dsc(1) - dsc(3)))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+    }
+}
+
+fun Canvas.drawBPLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBentPostLine(scale, w, h, paint)
+}
