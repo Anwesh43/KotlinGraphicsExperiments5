5bfcf75da2b980a3912a10e3483cfb53c6da50a1
Anwesh43 <anweshthecool0@gmail.com>
2025-02-20 01:54:04 +0530

    bentpostrotlineview: adding BestPostRotLine class

--- bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
+++ bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
@@ -157,4 +157,27 @@ class BentPostRotLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BentPostRotLine(var i : Int) {
+
+        private var curr : BPRLNode = BPRLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
