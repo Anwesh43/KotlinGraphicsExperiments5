f04d5758f61ce9b3beafc5e3e640e304ac29a4da
Anwesh43 <anweshthecool0@gmail.com>
2025-02-20 01:55:42 +0530

    bentpostrotlineview: adding Renderer class

--- bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
+++ bentpostrotlineview/src/main/java/com/example/bentpostrotlineview/BentPostRotLineView.kt
@@ -180,4 +180,27 @@ class BentPostRotLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BentPostRotLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val bprl : BentPostRotLine = BentPostRotLine(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bprl.draw(canvas, paint)
+            animator.animate {
+                bprl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bprl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
