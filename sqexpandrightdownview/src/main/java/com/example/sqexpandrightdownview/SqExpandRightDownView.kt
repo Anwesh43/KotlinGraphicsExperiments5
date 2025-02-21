a5d467af915f12c205070e15a1daff27cf2d6b92
Anwesh43 <anweshthecool0@gmail.com>
2025-02-22 03:58:36 +0530

    sqexpandrightdownview: adding SqExpandRightDownView class

--- sqexpandrightdownview/src/main/java/com/example/sqexpandrightdownview/SqExpandRightDownView.kt
+++ sqexpandrightdownview/src/main/java/com/example/sqexpandrightdownview/SqExpandRightDownView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawSERDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSqExpandRightDown(scale, w, h, paint)
 }
+
+class SqExpandRightDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
