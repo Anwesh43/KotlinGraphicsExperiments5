cd0ea5273e8bd988ea9bb35d1a34ca2b4d8dd9c9
Anwesh43 <anweshthecool0@gmail.com>
2025-02-22 04:13:40 +0530

    sqexpandrightdownview: adding Renderer class

--- sqexpandrightdownview/src/main/java/com/example/sqexpandrightdownview/SqExpandRightDownView.kt
+++ sqexpandrightdownview/src/main/java/com/example/sqexpandrightdownview/SqExpandRightDownView.kt
@@ -178,4 +178,27 @@ class SqExpandRightDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqExpandRightDownView) {
+
+        private val animator : Animator = Animator(view)
+        private var serd : SqExpandRightDown = SqExpandRightDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            serd.draw(canvas, paint)
+            animator.animate {
+                serd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            serd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
