025328533c21573006b0609efc227ac25fa91d42
Anwesh43 <anweshthecool0@gmail.com>
2025-02-24 01:08:10 +0530

    sqrotintoslideview: adding Renderer class

--- sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
+++ sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
@@ -182,4 +182,27 @@ class SqRotIntoSlideView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqRotIntoSlideView) {
+
+        private val animator : Animator = Animator(view)
+        private val sris : SqRotIntoSlide = SqRotIntoSlide(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sris.draw(canvas, paint)
+            animator.animate {
+                sris.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sris.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
