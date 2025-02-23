6d8cfaea9e88d1f3eca592306b4e4f4619f9c30a
Anwesh43 <anweshthecool0@gmail.com>
2025-02-23 10:02:48 +0530

    linehaflbentleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sqexpandrightdownview.SqExpandRightDownView
+import com.example.linehaflbentleftview.LineBentHalfLeftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SqExpandRightDownView.create(this)
+        LineBentHalfLeftView.create(this)
     }
 }
