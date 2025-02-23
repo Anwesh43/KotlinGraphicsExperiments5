135df65bdb3a7229e0d67b988176ac24b080c5b2
Anwesh43 <anweshthecool0@gmail.com>
2025-02-24 00:57:15 +0530

    sqrotintoslideview: adding SqRotIntoSlideView class

--- sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
+++ sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawSRISNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawSqRotIntoSlide(scale, w, h, paint)
 }
+
+class SqRotIntoSlideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
