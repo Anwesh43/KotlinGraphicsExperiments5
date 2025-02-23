e648bfbca53aa3e82d9b3f3e9849b63a5b9e588b
Anwesh43 <anweshthecool0@gmail.com>
2025-02-24 00:56:13 +0530

    sqrotintoslideview: adding Canvas extension methods

--- sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
+++ sqrotintoslideview/src/main/java/com/example/sqrotintoslideview/SqRotIntoSlideView.kt
@@ -26,3 +26,32 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqRotIntoSlide(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY((w / 2) * dsc(1), h / 2 - h * 0.5f * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2) * (2 * j - 1) * 2 * j)
+                drawRect(RectF(0f, 0f, size, size * dsc(0)), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSRISNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawSqRotIntoSlide(scale, w, h, paint)
+}
