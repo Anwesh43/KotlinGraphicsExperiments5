f4a686ad1a480ad33a480227c5b6b27e4e675cea
Anwesh43 <anweshthecool0@gmail.com>
2025-02-23 09:57:13 +0530

    linehaflbentleftview: adding LineBentHalfLeft class

--- linehaflbentleftview/src/main/java/com/example/linehaflbentleftview/LineBentHalfLeftView.kt
+++ linehaflbentleftview/src/main/java/com/example/linehaflbentleftview/LineBentHalfLeftView.kt
@@ -157,4 +157,27 @@ class LineBentHalfLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBentHalfLeft(var i : Int = 0) {
+
+        private var curr : LBHLNode = LBHLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
