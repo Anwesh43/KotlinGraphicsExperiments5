59955ca25bd05f0819d4cfa5c00558be8cdcb63f
Anwesh43 <anweshthecool0@gmail.com>
2025-02-25 00:35:08 +0530

    rottrijoinrightview: adding RotTriJoinRightView class

--- rottrijoinrightview/src/main/java/com/example/rottrijoinrightview/RotTriJoinRightView.kt
+++ rottrijoinrightview/src/main/java/com/example/rottrijoinrightview/RotTriJoinRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawRTJRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotTriJoinRight(scale, w, h, paint)
 }
+
+class RotTriJoinRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
