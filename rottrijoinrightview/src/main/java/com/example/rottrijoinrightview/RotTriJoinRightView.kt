fee912cdc1824ec4ebcb19d980af0387c3ce2e3c
Anwesh43 <anweshthecool0@gmail.com>
2025-02-25 00:45:50 +0530

    rottrijoinrightview: adding RTJRNode class

--- rottrijoinrightview/src/main/java/com/example/rottrijoinrightview/RotTriJoinRightView.kt
+++ rottrijoinrightview/src/main/java/com/example/rottrijoinrightview/RotTriJoinRightView.kt
@@ -122,4 +122,45 @@ class RotTriJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTJRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RTJRNode? = null
+        private var prev : RTJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RTJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTJRNode {
+            var curr : RTJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
