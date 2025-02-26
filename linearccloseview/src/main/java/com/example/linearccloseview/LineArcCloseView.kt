5da0a6910e24fdbbb7e7187074bee11e574a9de4
Anwesh43 <anweshthecool0@gmail.com>
2025-02-26 08:21:36 +0530

    linearccloseview: adding LACNode class

--- linearccloseview/src/main/java/com/example/linearccloseview/LineArcCloseView.kt
+++ linearccloseview/src/main/java/com/example/linearccloseview/LineArcCloseView.kt
@@ -120,4 +120,46 @@ class LineArcCloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LACNode? = null
+        private var prev : LACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LACNode {
+            var curr : LACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
