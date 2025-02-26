56e2b154dc66960d58644f698c3b1197142028ee
Anwesh43 <anweshthecool0@gmail.com>
2025-02-26 08:23:57 +0530

    linearccloseview: adding LineArcClose class

--- linearccloseview/src/main/java/com/example/linearccloseview/LineArcCloseView.kt
+++ linearccloseview/src/main/java/com/example/linearccloseview/LineArcCloseView.kt
@@ -160,6 +160,28 @@ class LineArcCloseView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class LineArcClose(var i : Int) {
+
+        private var curr : LACNode = LACNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
