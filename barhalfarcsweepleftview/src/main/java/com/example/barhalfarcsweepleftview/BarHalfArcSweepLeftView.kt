186bf4945a8d161d0a174c39bca25d9ac1290991
Anwesh43 <anweshthecool0@gmail.com>
2025-02-27 14:21:38 +0530

    barhalfarcsweepleftview: adding BarHalfArcSweepLeftView class

--- barhalfarcsweepleftview/src/main/java/com/example/barhalfarcsweepleftview/BarHalfArcSweepLeftView.kt
+++ barhalfarcsweepleftview/src/main/java/com/example/barhalfarcsweepleftview/BarHalfArcSweepLeftView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawBHSLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawBarHalfSweepLeft(scale, w, h, paint)
 }
+
+class BarHalfArcSweepLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
