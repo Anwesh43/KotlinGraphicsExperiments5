31ac39dabfa1bad0be6ba02fb0a859584a2f572a
Anwesh43 <anweshthecool0@gmail.com>
2025-02-27 14:47:55 +0530

    barhalfarcsweepleftview: adding static create method

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.barhalfarcsweepleftview.BarHalfArcSweepLeftView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linearccloseview.LineArcCloseView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineArcCloseView.create(this)
+        BarHalfArcSweepLeftView.create(this)
     }
 }
