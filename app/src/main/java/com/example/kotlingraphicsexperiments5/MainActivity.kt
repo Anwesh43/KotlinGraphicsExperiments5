b51d896f43850b9ea6bbdecb1aeeba8bee6297fb
Anwesh43 <anweshthecool0@gmail.com>
2025-02-28 12:47:43 +0530

    linefirstbentexpandview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.barhalfarcsweepleftview.BarHalfArcSweepLeftView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linefirstbentexpandview.LineFirstBentExpandView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BarHalfArcSweepLeftView.create(this)
+        LineFirstBentExpandView.create(this)
     }
 }
