eeb3ba9dd7f7185e3a8f8190b71f163c1ee9e6d0
Anwesh43 <anweshthecool0@gmail.com>
2025-02-28 12:30:49 +0530

    linefirstbentexpandview: adding LineFirstBentExpandView class

--- linefirstbentexpandview/src/main/java/com/example/linefirstbentexpandview/LineFirstBentExpandView.kt
+++ linefirstbentexpandview/src/main/java/com/example/linefirstbentexpandview/LineFirstBentExpandView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLFBENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineFirstBentExpand(scale, w, h, paint)
 }
+
+class LineFirstBentExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
