882be674057681a555513df31682b5916bde27a7
Anwesh43 <anweshthecool0@gmail.com>
2025-03-06 00:50:13 +0530

    linearcrotrightview: adding Renderer class

--- linearcrotrightview/src/main/java/com/example/linearcrotrightview/LineArcRotRightView.kt
+++ linearcrotrightview/src/main/java/com/example/linearcrotrightview/LineArcRotRightView.kt
@@ -184,4 +184,27 @@ class LineArcRotRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineArcRotRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val larr : LineArcRotRight = LineArcRotRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            larr.draw(canvas, paint)
+            animator.animate {
+                larr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            larr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
