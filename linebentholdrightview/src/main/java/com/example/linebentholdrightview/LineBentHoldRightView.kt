acf75c9653486e0e6bf04d7e8c8daae8bbf0b8d1
Anwesh43 <anweshthecool0@gmail.com>
2025-03-05 19:33:55 +0530

    linebentholdrightview: adding LBHRNode class

--- linebentholdrightview/src/main/java/com/example/linebentholdrightview/LineBentHoldRightView.kt
+++ linebentholdrightview/src/main/java/com/example/linebentholdrightview/LineBentHoldRightView.kt
@@ -120,4 +120,45 @@ class LineBentHoldRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBHRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBHRNode? = null
+        private var prev : LBHRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBHRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBHRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBHRNode {
+            var curr : LBHRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
