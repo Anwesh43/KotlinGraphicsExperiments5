5bf93cb478839b3511a816d26679bc2d0f081bba
Anwesh43 <anweshthecool0@gmail.com>
2025-03-12 22:25:37 +0530

    linebentjoinrightview: adding Animator class

--- linebentjoinrightview/src/main/java/com/example/linebentjoinrightview/LineBentJoinRightView.kt
+++ linebentjoinrightview/src/main/java/com/example/linebentjoinrightview/LineBentJoinRightView.kt
@@ -92,4 +92,32 @@ class LineBentJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
