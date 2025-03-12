90c37f9d48cdfb0aea5bc64055a403842a83414f
Anwesh43 <anweshthecool0@gmail.com>
2025-03-12 22:34:45 +0530

    linebentjoinrightview: adding LBJRNode class

--- linebentjoinrightview/src/main/java/com/example/linebentjoinrightview/LineBentJoinRightView.kt
+++ linebentjoinrightview/src/main/java/com/example/linebentjoinrightview/LineBentJoinRightView.kt
@@ -120,4 +120,45 @@ class LineBentJoinRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBJRNode(var i : Int, val state : State = State()) {
+
+        private var next : LBJRNode? = null
+        private var prev : LBJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBJRNode {
+            var curr : LBJRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
