080c451534edfb2de230a077225fe615ea440323
Anwesh43 <anweshthecool0@gmail.com>
2025-03-13 08:01:29 +0530

    linesixtyjoinarcview: adding LineSixtyJoinArcView class

--- linesixtyjoinarcview/src/main/java/com/example/linesixtyjoinarcview/LineSixtyJoinArcView.kt
+++ linesixtyjoinarcview/src/main/java/com/example/linesixtyjoinarcview/LineSixtyJoinArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLSJANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineSixtyJoinArc(scale, w, h, paint)
 }
+
+class LineSixtyJoinArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
