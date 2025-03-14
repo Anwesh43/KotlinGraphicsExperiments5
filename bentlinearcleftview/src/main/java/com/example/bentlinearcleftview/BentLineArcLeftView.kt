6c46f4701b0f122fcc3625ca20589cfafa3364de
Anwesh43 <anweshthecool0@gmail.com>
2025-03-14 09:02:04 +0530

    bentlinearcleftview: adding stroke style fix

--- bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
+++ bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
@@ -59,6 +59,7 @@ fun Canvas.drawBLALNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawBentLineArcLeft(scale, w, h, paint)
 }
 
