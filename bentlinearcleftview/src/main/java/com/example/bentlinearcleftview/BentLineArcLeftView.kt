c821d6c617712e76f361bc258bb8fb7052cc497d
Anwesh43 <anweshthecool0@gmail.com>
2025-03-14 08:38:55 +0530

    bentlinearcleftview: adding BLALNode class

--- bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
+++ bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
@@ -123,4 +123,45 @@ class BentLineArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLALNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLALNode? = null
+        private var prev : BLALNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLALNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLALNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLALNode {
+            var curr : BLALNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
