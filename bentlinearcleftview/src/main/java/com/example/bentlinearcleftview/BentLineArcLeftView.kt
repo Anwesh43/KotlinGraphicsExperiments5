456e78cab39ac221a229bc7093fc3110025fabd4
Anwesh43 <anweshthecool0@gmail.com>
2025-03-14 08:53:17 +0530

    bentlinearcleftview: adding static create method

--- bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
+++ bentlinearcleftview/src/main/java/com/example/bentlinearcleftview/BentLineArcLeftView.kt
@@ -213,4 +213,12 @@ class BentLineArcLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity: Activity) : BentLineArcLeftView {
+            val view : BentLineArcLeftView = BentLineArcLeftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
