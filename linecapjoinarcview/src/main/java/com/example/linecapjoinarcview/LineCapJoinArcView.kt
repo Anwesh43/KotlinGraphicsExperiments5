28a975bfdfe85d7a93dffc8e7040e97ea0a881c0
Anwesh43 <anweshthecool0@gmail.com>
2025-03-15 09:15:21 +0530

    linecapjoinarcview: adding Canvas extension methods

--- linecapjoinarcview/src/main/java/com/example/linecapjoinarcview/LineCapJoinArcView.kt
+++ linecapjoinarcview/src/main/java/com/example/linecapjoinarcview/LineCapJoinArcView.kt
@@ -28,3 +28,36 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineCapJoinArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * (dsc(3) + dsc(5)), h / 2) {
+        rotate(rot * dsc(4))
+        drawXY(0f, 0f) {
+            rotate(-deg)
+            drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        }
+        drawLine(size * (1 - dsc(0)), 0f, size, 0f, paint)
+        drawArc(RectF(-size, -size, size, size), -deg, deg * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLCJANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineCapJoinArc(scale, w, h, paint)
+}
