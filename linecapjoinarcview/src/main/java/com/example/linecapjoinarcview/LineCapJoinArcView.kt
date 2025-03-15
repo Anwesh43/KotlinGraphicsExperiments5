6b824bc9d7cbb216e58d4d6b37544c8755e934ab
Anwesh43 <anweshthecool0@gmail.com>
2025-03-15 09:16:48 +0530

    linecapjoinarcview: adding LineCapJoinArcView class

--- linecapjoinarcview/src/main/java/com/example/linecapjoinarcview/LineCapJoinArcView.kt
+++ linecapjoinarcview/src/main/java/com/example/linecapjoinarcview/LineCapJoinArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLCJANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineCapJoinArc(scale, w, h, paint)
 }
+
+class LineCapJoinArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
