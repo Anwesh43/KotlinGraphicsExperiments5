5f4f5d2fe9b050eaef9d5587823f2534d5939954
Anwesh43 <anweshthecool0@gmail.com>
2025-03-15 09:05:41 +0530

    linecapjoinarcview: adding constants and librareis

---
+++ linecapjoinarcview/src/main/java/com/example/linecapjoinarcview/LineCapJoinArcView.kt
@@ -0,0 +1,26 @@
+package com.example.linecapjoinarcview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
+val deg : Float = 45f
