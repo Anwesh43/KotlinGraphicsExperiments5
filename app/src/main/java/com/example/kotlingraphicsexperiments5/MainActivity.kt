2557d270e87dc38d4ec716252c2d80b2f8f5b415
Anwesh43 <anweshthecool0@gmail.com>
2025-03-15 09:41:56 +0530

    linecapjoinarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bentlinearcleftview.BentLineArcLeftView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linecapjoinarcview.LineCapJoinArcView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineArcLeftView.create(this)
+        LineCapJoinArcView.create(this)
     }
 }
