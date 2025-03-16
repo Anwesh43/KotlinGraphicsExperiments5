1da06aaa69bdc725065a598a71350d76d9f986c2
Anwesh43 <anweshthecool0@gmail.com>
2025-03-16 08:38:07 +0530

    upjoinarcrightview: adding UJARNode class

--- upjoinarcrightview/src/main/java/com/example/upjoinarcrightview/UpJoinArcRightView.kt
+++ upjoinarcrightview/src/main/java/com/example/upjoinarcrightview/UpJoinArcRightView.kt
@@ -118,4 +118,45 @@ class UpJoinArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class UJARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : UJARNode? = null
+        private var prev : UJARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = UJARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawUJARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : UJARNode {
+            var curr : UJARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
