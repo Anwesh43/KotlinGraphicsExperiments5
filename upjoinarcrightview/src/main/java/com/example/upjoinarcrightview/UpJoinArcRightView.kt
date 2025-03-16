8a41e1156c5d4299dc2e0395f3073029dc2c1d82
Anwesh43 <anweshthecool0@gmail.com>
2025-03-16 08:54:04 +0530

    upjoinarcrightview: adding fix

--- upjoinarcrightview/src/main/java/com/example/upjoinarcrightview/UpJoinArcRightView.kt
+++ upjoinarcrightview/src/main/java/com/example/upjoinarcrightview/UpJoinArcRightView.kt
@@ -53,6 +53,7 @@ fun Canvas.drawUJARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawUpJoinArcRight(scale, w, h, paint)
 }
 
