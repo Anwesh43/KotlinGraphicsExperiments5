4b2d96634b571fa5e42ef8b5f033134cc96ac1df
Anwesh43 <anweshthecool0@gmail.com>
2025-03-16 08:53:17 +0530

    upjoinarcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linecapjoinarcview.LineCapJoinArcView
+import com.example.upjoinarcrightview.UpJoinArcRightView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineCapJoinArcView.create(this)
+        UpJoinArcRightView.create(this)
     }
 }
