92d461c95c9db602a60677b21293aad286a64342
Anwesh43 <anweshthecool0@gmail.com>
2025-03-18 03:09:33 +0530

    sequencelinearcdownview: adding SequenceLineArcDownView class

--- sequencelinearcdownview/src/main/java/com/example/sequencelinearcdownview/SequenceLineArcDownView.kt
+++ sequencelinearcdownview/src/main/java/com/example/sequencelinearcdownview/SequenceLineArcDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSLADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSequenceLineArcDown(scale, w, h, paint)
 }
+
+class SequenceLineArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
