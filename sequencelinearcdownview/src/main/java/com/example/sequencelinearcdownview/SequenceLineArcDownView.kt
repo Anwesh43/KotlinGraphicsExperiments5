37182c059f7745090b629de374e8de501c343942
Anwesh43 <anweshthecool0@gmail.com>
2025-03-18 03:08:03 +0530

    sequencelinearcdownview: adding Canvas extension methods

--- sequencelinearcdownview/src/main/java/com/example/sequencelinearcdownview/SequenceLineArcDownView.kt
+++ sequencelinearcdownview/src/main/java/com/example/sequencelinearcdownview/SequenceLineArcDownView.kt
@@ -27,3 +27,37 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSequenceLineArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            val ds1j : Float = dsc(j).divideScale(0, 2)
+            val ds2j : Float = dsc(j).divideScale(1, 2)
+            drawXY(size * j, 0f) {
+                drawLine(0f, 0f, 0f, -size * ds1j, paint)
+                drawArc(RectF(-size, -size, size, size), -90f, 90f * ds2j, false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSLADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSequenceLineArcDown(scale, w, h, paint)
+}
