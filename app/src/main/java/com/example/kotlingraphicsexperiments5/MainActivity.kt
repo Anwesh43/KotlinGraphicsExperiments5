6b28d0e36569cadd4c2e2f97f46ef2b0145ea630
Anwesh43 <anweshthecool0@gmail.com>
2025-03-18 03:24:03 +0530

    sequencelinearcdownview: adding view in acitvity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.downrightlinerotview.DownRightLineRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.sequencelinearcdownview.SequenceLineArcDownView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DownRightLineRotView.create(this)
+        SequenceLineArcDownView.create(this)
     }
 }
