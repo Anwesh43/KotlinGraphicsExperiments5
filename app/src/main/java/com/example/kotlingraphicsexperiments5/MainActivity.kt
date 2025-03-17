fe0ff62b12c5064709600ce165d48e67b1342cc0
Anwesh43 <anweshthecool0@gmail.com>
2025-03-17 09:54:15 +0530

    downrightlinerotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.downrightlinerotview.DownRightLineRotView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.upjoinarcrightview.UpJoinArcRightView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        UpJoinArcRightView.create(this)
+        DownRightLineRotView.create(this)
     }
 }
