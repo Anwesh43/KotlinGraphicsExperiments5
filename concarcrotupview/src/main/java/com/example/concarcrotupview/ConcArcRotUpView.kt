94bdff8868082360d7d9ee530f446667cab82e7a
Anwesh43 <anweshthecool0@gmail.com>
2025-03-19 00:56:36 +0530

    concarcrotupview: adding Int and Float extension methods

--- concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
+++ concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
@@ -26,3 +26,6 @@ val rot : Float = 180f
 val startDeg : Float = 60f
 val sweep : Float = 60f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
