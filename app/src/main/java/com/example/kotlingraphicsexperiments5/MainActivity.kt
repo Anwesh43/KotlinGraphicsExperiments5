fdb205b36cdee6702f3c6836ffaf6628842a53a1
Anwesh43 <anweshthecool0@gmail.com>
2025-03-19 01:46:28 +0530

    concarcrotupview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.concarcrotupview.ConcArcRotUpView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.sequencelinearcdownview.SequenceLineArcDownView
+
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SequenceLineArcDownView.create(this)
+        ConcArcRotUpView.create(this)
     }
 }
