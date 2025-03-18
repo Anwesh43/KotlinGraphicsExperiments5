5cf391334d7527174b682d39f17bb890b4df348d
Anwesh43 <anweshthecool0@gmail.com>
2025-03-19 01:13:01 +0530

    concarcrotupview: adding Canvas extension methods

--- concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
+++ concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
@@ -29,3 +29,33 @@ val sweep : Float = 60f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawConcArcRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            val r : Float = size - size * 0.33f * j
+            drawArc(RectF(-r, -r, r, r), startDeg, sweep, false, paint)
+        }
+    }
+}
+
+fun Canvas.drawCARUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawConcArcRotUp(scale, w, h, paint)
+}
