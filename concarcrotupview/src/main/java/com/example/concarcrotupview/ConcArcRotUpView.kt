c773eb3c740f69bbe1423195ad2c6264f10d77a1
Anwesh43 <anweshthecool0@gmail.com>
2025-03-19 01:29:18 +0530

    concarcrotupview: adding CARUNode class

--- concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
+++ concarcrotupview/src/main/java/com/example/concarcrotupview/ConcArcRotUpView.kt
@@ -122,4 +122,45 @@ class ConcArcRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CARUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : CARUNode? = null
+        private var prev : CARUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CARUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCARUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CARUNode {
+            var curr : CARUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
