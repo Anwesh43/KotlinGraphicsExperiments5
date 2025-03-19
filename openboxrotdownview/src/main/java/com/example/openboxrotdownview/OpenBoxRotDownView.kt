5b638009d606c879b6c4e53e5d18699a634cfb10
Anwesh43 <anweshthecool0@gmail.com>
2025-03-20 01:32:09 +0530

    openboxrotdownview: adding OpenBoxRotDownView class

--- openboxrotdownview/src/main/java/com/example/openboxrotdownview/OpenBoxRotDownView.kt
+++ openboxrotdownview/src/main/java/com/example/openboxrotdownview/OpenBoxRotDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawOBRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawOpenBoxRotDown(scale, w, h, paint)
 }
+
+class OpenBoxRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
