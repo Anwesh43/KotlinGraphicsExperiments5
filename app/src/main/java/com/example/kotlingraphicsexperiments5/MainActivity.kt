c2938eef7e0999e3a3038a193d91f46198e0b3b8
Anwesh43 <anweshthecool0@gmail.com>
2025-03-22 20:15:40 +0530

    linetouchrotaltview: adding view in acitivity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.openboxrotdownview.OpenBoxRotDownView
+import com.example.linetouchrotaltview.LineTouchRotAltView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        OpenBoxRotDownView.create(this)
+        LineTouchRotAltView.create(this)
     }
 }
