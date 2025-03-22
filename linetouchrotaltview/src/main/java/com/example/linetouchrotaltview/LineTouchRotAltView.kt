e71b5e4b02f8d7b272750742884c0d946a4c7780
Anwesh43 <anweshthecool0@gmail.com>
2025-03-22 19:17:44 +0530

    linetouchrotaltview: adding Canvas extension methods

--- linetouchrotaltview/src/main/java/com/example/linetouchrotaltview/LineTouchRotAltView.kt
+++ linetouchrotaltview/src/main/java/com/example/linetouchrotaltview/LineTouchRotAltView.kt
@@ -26,3 +26,39 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineTouchRotAlt(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            val fac : Float = 1f - 2 * j
+            drawXY(0f, h * 0.5f * fac * dsc(3)) {
+                drawLine(0f, 0f, size * dsc(0) * fac, 0f, paint)
+                drawXY(size * (1f - 2 * j), -h * 0.5f * fac * (1f - dsc(1))) {
+                    rotate(rot * fac * j * dsc(2))
+                    drawLine(0f, 0f, 0f, -size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLTRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineTouchRotAlt(scale, w, h, paint)
+}
