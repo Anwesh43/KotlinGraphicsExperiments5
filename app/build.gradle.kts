54e80b2f7e86720f53de350971a4d123747eb2ba
Anwesh43 <anweshthecool0@gmail.com>
2025-03-24 12:34:35 +0530

    linealtarrowview: adding view in activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":arccompleteinmiddleview"))
+    implementation(project(":linealtarrowview"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arccompleteinmiddleview.ArcCompleteInMiddleView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linealtarrowview.LineAltArrowView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ArcCompleteInMiddleView.create(this)
+        LineAltArrowView.create(this)
     }
 }
