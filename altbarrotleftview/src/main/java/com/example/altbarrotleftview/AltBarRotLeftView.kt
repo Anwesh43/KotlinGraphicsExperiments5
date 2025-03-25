e23c45f132e447b8ffec5c1aa55504c040f78be6
Anwesh43 <anweshthecool0@gmail.com>
2025-03-25 11:44:40 +0530

    altbarrotleftview: adding AltBarRotLeftView class

--- altbarrotleftview/src/main/java/com/example/altbarrotleftview/AltBarRotLeftView.kt
+++ altbarrotleftview/src/main/java/com/example/altbarrotleftview/AltBarRotLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawABRLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawAltBarRotLeft(scale, w, h, paint)
 }
+
+class AltBarRotLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
