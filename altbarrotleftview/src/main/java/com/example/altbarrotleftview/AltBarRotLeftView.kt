fa0c1f2ab20ac82b474d9abebeaf4d6ca5950e0e
Anwesh43 <anweshthecool0@gmail.com>
2025-03-25 11:52:33 +0530

    altbarrotleftview: adding ABRLNode class

--- altbarrotleftview/src/main/java/com/example/altbarrotleftview/AltBarRotLeftView.kt
+++ altbarrotleftview/src/main/java/com/example/altbarrotleftview/AltBarRotLeftView.kt
@@ -122,4 +122,45 @@ class AltBarRotLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ABRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ABRLNode? = null
+        private var prev : ABRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ABRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawABRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ABRLNode {
+            var curr : ABRLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
