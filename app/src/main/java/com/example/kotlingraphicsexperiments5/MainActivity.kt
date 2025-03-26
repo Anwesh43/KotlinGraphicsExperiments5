34a39857f1ae1437642bca360be7ab5843d7d0e8
Anwesh43 <anweshthecool0@gmail.com>
2025-03-26 11:25:06 +0530

    lineendtrirotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.altbarrotleftview.AltBarRotLeftView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.lineendtrirotview.LineEndTriRotView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AltBarRotLeftView.create(this)
+        LineEndTriRotView.create(this)
     }
 }
