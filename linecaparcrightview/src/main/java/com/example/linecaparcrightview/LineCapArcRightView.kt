1c135729654ac54a7031970559dbcf803ee579cb
Anwesh43 <anweshthecool0@gmail.com>
2025-03-30 11:52:35 +0530

    linecaparcrightview: adding Canvas extension methods

--- linecaparcrightview/src/main/java/com/example/linecaparcrightview/LineCapArcRightView.kt
+++ linecaparcrightview/src/main/java/com/example/linecaparcrightview/LineCapArcRightView.kt
@@ -8,6 +8,7 @@ import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
 import android.graphics.Color
+import com.google.android.material.color.ColorRoles
 
 val colors : Array<String> = arrayOf(
     "#1A237E",
@@ -27,3 +28,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineCapArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawXY(-w / 2 + (w / 2) * dsc(2), 0f) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 90f * dsc(0), false, paint)
+            drawLine(size, 0f, size * (1 - dsc(1)), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLCARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineCapArcRight(scale, w, h, paint)
+}
