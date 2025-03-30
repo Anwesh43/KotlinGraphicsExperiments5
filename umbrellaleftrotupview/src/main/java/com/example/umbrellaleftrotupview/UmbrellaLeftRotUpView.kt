f3b16fcb6c0ad3254ce3cd4eb538103bee59e5fa
Anwesh43 <anweshthecool0@gmail.com>
2025-03-31 02:58:38 +0530

    umbrellaleftrotupview: adding UmbrellaLeftRotUpView class

--- umbrellaleftrotupview/src/main/java/com/example/umbrellaleftrotupview/UmbrellaLeftRotUpView.kt
+++ umbrellaleftrotupview/src/main/java/com/example/umbrellaleftrotupview/UmbrellaLeftRotUpView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawULRUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawUmbrellaLeftRotUp(scale, w, h, paint)
 }
+
+class UmbrellaLeftRotUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
