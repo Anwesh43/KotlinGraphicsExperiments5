3c8d8504de87fee7ed68ea3b36902d641b38e389
Anwesh43 <anweshthecool0@gmail.com>
2025-04-02 05:12:30 +0530

    bentlineoneeightyview: adding BentLineOneEight class

--- bentlineoneeightyview/src/main/java/com/example/bentlineoneeightyview/BentLineOneEightyView.kt
+++ bentlineoneeightyview/src/main/java/com/example/bentlineoneeightyview/BentLineOneEightyView.kt
@@ -162,4 +162,27 @@ class BentLineOneEightyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BentLineOneEight(var i : Int) {
+
+        private var curr : BLOENode = BLOENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
