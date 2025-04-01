c78ab04798da31645f3ee634666cdf038b196731
Anwesh43 <anweshthecool0@gmail.com>
2025-04-02 05:01:27 +0530

    bentlineoneeightyview: adding BentLineOneEightyView class

--- bentlineoneeightyview/src/main/java/com/example/bentlineoneeightyview/BentLineOneEightyView.kt
+++ bentlineoneeightyview/src/main/java/com/example/bentlineoneeightyview/BentLineOneEightyView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBLOENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBentlineOneEighty(scale, w, h, paint)
 }
+
+class BentLineOneEightyView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
