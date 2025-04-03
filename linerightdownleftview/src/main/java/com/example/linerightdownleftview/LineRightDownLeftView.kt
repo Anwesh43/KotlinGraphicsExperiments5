136b3c40c04e6572795a3846b894c0c3813b3da5
Anwesh43 <anweshthecool0@gmail.com>
2025-04-03 10:25:53 +0530

    linerightdownleftview: adding LRDLNode class

--- linerightdownleftview/src/main/java/com/example/linerightdownleftview/LineRightDownLeftView.kt
+++ linerightdownleftview/src/main/java/com/example/linerightdownleftview/LineRightDownLeftView.kt
@@ -119,4 +119,45 @@ class LineRightDownLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRDLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRDLNode? = null
+        private var prev : LRDLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRDLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRDLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRDLNode {
+            var curr : LRDLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
