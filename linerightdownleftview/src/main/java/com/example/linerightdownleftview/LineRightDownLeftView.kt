c8ccff198b7d9468cbec06a98d5735fd6522d23e
Anwesh43 <anweshthecool0@gmail.com>
2025-04-03 10:12:02 +0530

    linerightdownleftview: adding Canvas extension methods

--- linerightdownleftview/src/main/java/com/example/linerightdownleftview/LineRightDownLeftView.kt
+++ linerightdownleftview/src/main/java/com/example/linerightdownleftview/LineRightDownLeftView.kt
@@ -26,3 +26,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightDownLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * (dsc(2) - dsc(4)), h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawLRDLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightDownLeft(scale, w, h, paint)
+}
