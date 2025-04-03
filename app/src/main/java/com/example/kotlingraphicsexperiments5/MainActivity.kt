621efbd2e11f009a4ef6e44039410b99af139a71
Anwesh43 <anweshthecool0@gmail.com>
2025-04-03 10:39:32 +0530

    linerightdownleftview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.bentlineoneeightyview.BentLineOneEightyView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
+import com.example.linerightdownleftview.LineRightDownLeftView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BentLineOneEightyView.create(this)
+        LineRightDownLeftView.create(this)
     }
 }
