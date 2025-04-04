87646af8b57a20cbed5dec3ee169c475c328d53d
Anwesh43 <anweshthecool0@gmail.com>
2025-04-04 15:27:22 +0530

    rotabovelineupview: adding RotAboveLineUp class

--- rotabovelineupview/src/main/java/com/example/rotabovelineupview/RotAboveLineUpView.kt
+++ rotabovelineupview/src/main/java/com/example/rotabovelineupview/RotAboveLineUpView.kt
@@ -167,4 +167,27 @@ class RotAboveLineUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotAboveLineUp(var i : Int) {
+
+        private var curr : RALUNode = RALUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
