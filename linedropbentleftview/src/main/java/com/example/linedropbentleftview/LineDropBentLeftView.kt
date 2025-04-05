f96fa0bb05b39350461c06491e30d900517b3404
Anwesh43 <anweshthecool0@gmail.com>
2025-04-05 16:26:40 +0530

    linedropbentleftview: adding LDBLNode class

--- linedropbentleftview/src/main/java/com/example/linedropbentleftview/LineDropBentLeftView.kt
+++ linedropbentleftview/src/main/java/com/example/linedropbentleftview/LineDropBentLeftView.kt
@@ -122,4 +122,45 @@ class LineDropBentLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDBLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDBLNode? = null
+        private var prev : LDBLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDBLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDBLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDBLNode {
+            var curr : LDBLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
