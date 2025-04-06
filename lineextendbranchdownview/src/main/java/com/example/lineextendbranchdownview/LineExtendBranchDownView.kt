eedb14a639799d713d4448ff7e4c6157abc6ef1b
Anwesh43 <anweshthecool0@gmail.com>
2025-04-06 10:54:15 +0530

    lineextendbranchdownview: adding static create method

--- lineextendbranchdownview/src/main/java/com/example/lineextendbranchdownview/LineExtendBranchDownView.kt
+++ lineextendbranchdownview/src/main/java/com/example/lineextendbranchdownview/LineExtendBranchDownView.kt
@@ -215,4 +215,13 @@ class LineExtendBranchDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : LineExtendBranchDownView {
+            val view : LineExtendBranchDownView = LineExtendBranchDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
