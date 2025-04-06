12ea26ce0cf1fc73e14c0ef90b50e7480086cfb0
Anwesh43 <anweshthecool0@gmail.com>
2025-04-06 10:30:48 +0530

    lineextendbranchdownview: adding Canvas extension methods

--- lineextendbranchdownview/src/main/java/com/example/lineextendbranchdownview/LineExtendBranchDownView.kt
+++ lineextendbranchdownview/src/main/java/com/example/lineextendbranchdownview/LineExtendBranchDownView.kt
@@ -27,3 +27,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExtendBranchDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * dsc(2), h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            val dsj1 : Float = dsc(j).divideScale(0, 2)
+            val dsj2 : Float = dsc(j).divideScale(1, 2)
+            drawXY(size * 0.5f * j, 0f) {
+                drawLine(0f, 0f, size * 0.5f * dsj1, 0f, paint)
+                drawXY(size / 2, 0f) {
+                    drawLine(0f, 0f, 0f, size * 0.5f * (1f - 2 * j) * dsj2, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLEBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExtendBranchDown(scale, w, h, paint)
+}
