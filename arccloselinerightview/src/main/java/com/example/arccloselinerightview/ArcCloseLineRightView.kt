d28bef9b2bc1fac2fb7c1c5c963b945834a7dff7
Anwesh43 <anweshthecool0@gmail.com>
2025-04-07 09:44:26 +0530

    arccloselinerightview: adding fix

--- arccloselinerightview/src/main/java/com/example/arccloselinerightview/ArcCloseLineRightView.kt
+++ arccloselinerightview/src/main/java/com/example/arccloselinerightview/ArcCloseLineRightView.kt
@@ -56,6 +56,7 @@ fun Canvas.drawACLRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawArcCloseLineRight(scale, w, h, paint)
 }
 
