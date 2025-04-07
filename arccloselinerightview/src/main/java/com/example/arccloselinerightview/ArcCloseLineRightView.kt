a046c03cca8ff124285e0687e76189bc820a50fb
Anwesh43 <anweshthecool0@gmail.com>
2025-04-07 09:30:22 +0530

    arccloselinerightview: adding ACLRNode class

--- arccloselinerightview/src/main/java/com/example/arccloselinerightview/ArcCloseLineRightView.kt
+++ arccloselinerightview/src/main/java/com/example/arccloselinerightview/ArcCloseLineRightView.kt
@@ -121,4 +121,45 @@ class ArcCloseLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ACLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ACLRNode? = null
+        private var prev : ACLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ACLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACLRNode {
+            var curr : ACLRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
