fa6d64fc7f1c5fb06e4b0e7a8323ec5e4a47e530
Anwesh43 <anweshthecool0@gmail.com>
2025-04-08 12:02:34 +0530

    barrotrightdownview: adding BarRotRightDownView class

--- barrotrightdownview/src/main/java/com/example/barrotrightdownview/BarRotRightDownView.kt
+++ barrotrightdownview/src/main/java/com/example/barrotrightdownview/BarRotRightDownView.kt
@@ -53,3 +53,19 @@ fun Canvas.drawBRRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawBarRotRightDown(scale, w, h, paint)
 }
+
+class BrRotRightDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
