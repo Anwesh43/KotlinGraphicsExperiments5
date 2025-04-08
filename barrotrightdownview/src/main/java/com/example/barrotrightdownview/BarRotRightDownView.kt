776dd501509b218383d79f6d6df17483d2d27974
Anwesh43 <anweshthecool0@gmail.com>
2025-04-08 12:16:07 +0530

    barrotrightdownview: adding BRRDNode class

--- barrotrightdownview/src/main/java/com/example/barrotrightdownview/BarRotRightDownView.kt
+++ barrotrightdownview/src/main/java/com/example/barrotrightdownview/BarRotRightDownView.kt
@@ -116,4 +116,45 @@ class BrRotRightDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BRRDNode? = null
+        private var prev : BRRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRRDNode {
+            var curr : BRRDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
