b590820159d12f2424697b96634510ce9605777d
Anwesh43 <anweshthecool0@gmail.com>
2025-04-09 12:40:37 +0530

    linebentclearexpandview: adding LBCENode class

--- linebentclearexpandview/src/main/java/com/example/linebentclearexpandview/LineBentClearExpandView.kt
+++ linebentclearexpandview/src/main/java/com/example/linebentclearexpandview/LineBentClearExpandView.kt
@@ -119,4 +119,45 @@ class LineBentClearExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBCENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBCENode? = null
+        private var prev : LBCENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBCENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBCENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBCENode {
+            var curr : LBCENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
