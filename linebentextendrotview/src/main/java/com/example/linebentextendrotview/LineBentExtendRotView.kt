25746d7dac7e3390fdb58c77c54cd6cf45e3c6bb
Anwesh43 <anweshthecool0@gmail.com>
2025-04-10 11:22:24 +0530

    linebentextendrotview: adding Renderer class

--- linebentextendrotview/src/main/java/com/example/linebentextendrotview/LineBentExtendRotView.kt
+++ linebentextendrotview/src/main/java/com/example/linebentextendrotview/LineBentExtendRotView.kt
@@ -183,4 +183,27 @@ class LineBentExtendRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentExtendRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val lber : LineBentExtendRot = LineBentExtendRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lber.draw(canvas, paint)
+            animator.animate {
+                lber.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lber.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
