2b2601e22b3001e2f0a765b2445117bc4ab20a26
Anwesh43 <anweshthecool0@gmail.com>
2025-04-11 11:44:01 +0530

    arccompleterotleftview: adding Renderer class

--- arccomplerotleftview/src/main/java/com/example/arccomplerotleftview/ArcCompleteExtendRotView.kt
+++ arccomplerotleftview/src/main/java/com/example/arccomplerotleftview/ArcCompleteExtendRotView.kt
@@ -182,4 +182,27 @@ class ArcCompleteExtendRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcCompleteExtendRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val acer : ArcCompleteExtendRot = ArcCompleteExtendRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            acer.draw(canvas, paint)
+            animator.animate {
+                acer.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            acer.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
