7a37bc470c4fd278d5c6bf7feabcbcb5b7ea1a69
Anwesh43 <anweshthecool0@gmail.com>
2025-04-11 10:58:39 +0530

    arccompleterotleftview: adding Canvas extension methods

--- arccomplerotleftview/src/main/java/com/example/arccomplerotleftview/ArcCompleteExtendRotView.kt
+++ arccomplerotleftview/src/main/java/com/example/arccomplerotleftview/ArcCompleteExtendRotView.kt
@@ -27,3 +27,31 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcCompleteExtendRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * (dsc(1) - dsc(4)), h / 2) {
+        rotate(rot * dsc(2))
+        val degSweep : Float = 180f * (dsc(0) + dsc(3))
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f - degSweep, degSweep, false, paint)
+    }
+}
+
+fun Canvas.drawACERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcCompleteExtendRot(scale, w, h, paint)
+}
