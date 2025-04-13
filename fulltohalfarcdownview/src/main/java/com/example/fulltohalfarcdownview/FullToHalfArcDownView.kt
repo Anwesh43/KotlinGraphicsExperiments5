993a1c2a6887c19199b4cacfca94e4bacd6df64f
Anwesh43 <anweshthecool0@gmail.com>
2025-04-13 17:13:38 +0530

    fulltohalfarcdownview: adding Canvas extension methods

--- fulltohalfarcdownview/src/main/java/com/example/fulltohalfarcdownview/FullToHalfArcDownView.kt
+++ fulltohalfarcdownview/src/main/java/com/example/fulltohalfarcdownview/FullToHalfArcDownView.kt
@@ -27,3 +27,37 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXy(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawFullToHalfArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(0f, -size / 2, size, size / 2), 180f * dsc(3), 180f * (2 * dsc(0) - dsc(3)), false, paint)
+    }
+}
+
+fun Canvas.drawFTHADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFullToHalfArcDown(scale, w, h, paint)
+}
