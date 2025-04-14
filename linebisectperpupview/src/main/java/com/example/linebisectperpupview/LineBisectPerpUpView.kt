fc9794309e635325a7fb6025b69729749d434d07
Anwesh43 <anweshthecool0@gmail.com>
2025-04-14 16:12:47 +0530

    linebisectperpupview: adding LBPUNode class

--- linebisectperpupview/src/main/java/com/example/linebisectperpupview/LineBisectPerpUpView.kt
+++ linebisectperpupview/src/main/java/com/example/linebisectperpupview/LineBisectPerpUpView.kt
@@ -120,4 +120,45 @@ class LineBisectPerpUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBPUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBPUNode? = null
+        private var prev : LBPUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBPUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBPUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBPUNode {
+            var curr : LBPUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
