f83cab47039b0860590415442382f06a8b4927db
Anwesh43 <anweshthecool0@gmail.com>
2025-04-15 12:27:32 +0530

    linetricirclecapview: adding LineTriCIRCLECapView class

--- linetricirclecapview/src/main/java/com/example/linetricirclecapview/LineTriCircleCapView.kt
+++ linetricirclecapview/src/main/java/com/example/linetricirclecapview/LineTriCircleCapView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLTCCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineTriCircleCap(scale, w, h, paint)
 }
+
+class LineTriCircleCapView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
