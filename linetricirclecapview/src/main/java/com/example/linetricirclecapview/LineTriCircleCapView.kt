476a74566e1ae4f7c26d8ef179476ffd1bae736d
Anwesh43 <anweshthecool0@gmail.com>
2025-04-15 11:41:50 +0530

    linetricirclecapview: adding Canvas extension methods

--- linetricirclecapview/src/main/java/com/example/linetricirclecapview/LineTriCircleCapView.kt
+++ linetricirclecapview/src/main/java/com/example/linetricirclecapview/LineTriCircleCapView.kt
@@ -27,3 +27,40 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineTriCircleCap(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        rotate(-rot * dsc(3))
+        val dsc00 : Float = dsc(0).divideScale(0, 2)
+        val dsc01 : Float = dsc(0).divideScale(1, 2)
+        drawLine(0f, 0f, size * 0.5f * dsc00, size * 0.5f * dsc00, paint)
+        drawXY(size / 2, size / 2) {
+            drawLine(0f, 0f, size * 0.5f * dsc01, -size * 0.5f * dsc01, paint)
+        }
+        drawXY(size, 0f) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLTCCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineTriCircleCap(scale, w, h, paint)
+}
