3897753b6beef2277a15928f387ef7151e9a6dc6
Anwesh43 <anweshthecool0@gmail.com>
2025-04-16 17:49:38 +0530

    perplineanglerightview: adding PerpLineAngleRightView class

--- perplineanglerightview/src/main/java/com/example/perplineanglerightview/PerpLineAngleRightView.kt
+++ perplineanglerightview/src/main/java/com/example/perplineanglerightview/PerpLineAngleRightView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawPLARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawPerpLineAngleRight(scale, w, h, paint)
 }
+
+class PerpLineAngleRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
