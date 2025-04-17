187ba521375404dfb0083e22d2cd6cc4a7e6d964
Anwesh43 <anweshthecool0@gmail.com>
2025-04-17 16:14:25 +0530

    linerightsweeparcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.perplineanglerightview.PerpLineAngleRightView
+import com.example.linerightsweeparcview.LineRightSweepArcView
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PerpLineAngleRightView.create(this)
+        LineRightSweepArcView.create(this)
     }
 }
