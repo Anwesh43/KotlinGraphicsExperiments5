579bd30dabe2c9c215a1d458b8d1dfcca39ab4bc
Anwesh43 <anweshthecool0@gmail.com>
2025-04-18 19:58:17 +0530

    linedropperpdownview: adding LDPDNode class

--- linedropperpdownview/src/main/java/com/example/linedropperpdownview/LineDropPerpDownView.kt
+++ linedropperpdownview/src/main/java/com/example/linedropperpdownview/LineDropPerpDownView.kt
@@ -119,4 +119,45 @@ class LineDropPerpDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDPDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDPDNode? = null
+        private var prev : LDPDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDPDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDPDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDPDNode {
+            var curr : LDPDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
