9faf10542f78e2f846eb6b140abf8959476d06ec
Anwesh43 <anweshthecool0@gmail.com>
2025-04-21 09:14:17 +0530

    alteperpjoinarcview: adding APJANode class

--- altperpjoinarcview/src/main/java/com/example/altperpjoinarcview/AltPerpJoinArcView.kt
+++ altperpjoinarcview/src/main/java/com/example/altperpjoinarcview/AltPerpJoinArcView.kt
@@ -125,4 +125,45 @@ class AltPerpJoinArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class APJANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : APJANode? = null
+        private var prev : APJANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = APJANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAPJANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : APJANode {
+            var curr : APJANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
