f854becaf1f141bb038a07b90145e39f70db2f11
Anwesh43 <anweshthecool0@gmail.com>
2025-04-22 07:50:07 +0530

    rotarcleftdownview: adding RALDNode class

--- rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
+++ rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
@@ -117,5 +117,46 @@ class RotArcLeftDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RALDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RALDNode? = null
+        private var prev : RALDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RALDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRALDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RALDNode {
+            var curr : RALDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
