6adf942d93ba6eb31f36de4043aa16b8fba276b8
Anwesh43 <anweshthecool0@gmail.com>
2025-04-22 07:40:23 +0530

    rotarcleftdownview: adding RotArcLeftDownView class

--- rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
+++ rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawRALDNode(i : Int, scale : Float, paint : Paint) {
     drawRotArcLeftDown(scale, w, h, paint)
 }
 
+class RotArcLeftDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
+
