e48dba8ae82494aa832a34232bba1fe4e6402528
Anwesh43 <anweshthecool0@gmail.com>
2025-04-22 08:13:48 +0530

    rotarcleftdownview: adding fix

--- rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
+++ rotarcleftdownview/src/main/java/com/example/rotarcleftdownview/RotArcLeftDownView.kt
@@ -52,6 +52,7 @@ fun Canvas.drawRALDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawRotArcLeftDown(scale, w, h, paint)
 }
 
