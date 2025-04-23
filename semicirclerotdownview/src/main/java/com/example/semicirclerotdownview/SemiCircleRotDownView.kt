712ea0135b95ac884b9007660658a8d805d0f0f9
Anwesh43 <anweshthecool0@gmail.com>
2025-04-24 02:58:26 +0530

    semicirclerotdownview: adding State class

--- semicirclerotdownview/src/main/java/com/example/semicirclerotdownview/SemiCircleRotDownView.kt
+++ semicirclerotdownview/src/main/java/com/example/semicirclerotdownview/SemiCircleRotDownView.kt
@@ -70,4 +70,24 @@ class SemiCircleRotDownView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 9f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir === 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
