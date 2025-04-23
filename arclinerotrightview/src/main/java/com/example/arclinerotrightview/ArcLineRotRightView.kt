58cbcad3b6e52faab9febd320d2753875f23898a
Anwesh43 <anweshthecool0@gmail.com>
2025-04-23 09:55:23 +0530

    arclinerotrightview: adding ArcLineRotRightView class

--- arclinerotrightview/src/main/java/com/example/arclinerotrightview/ArcLineRotRightView.kt
+++ arclinerotrightview/src/main/java/com/example/arclinerotrightview/ArcLineRotRightView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawALRRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcLineRotRight(scale, w, h, paint)
 }
+
+class ArcLineRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
