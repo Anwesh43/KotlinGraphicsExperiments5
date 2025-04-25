2372c7d85b2cedde895ee542acaf405f3c4c84d6
Anwesh43 <anweshthecool0@gmail.com>
2025-04-25 10:42:18 +0530

    linebreakslantleftview: adding view in acitivity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -12,12 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.semicirclerotdownview.SemiCircleRotDownView
-
+import com.example.linebreakslantleftview.LineBreakSlantLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SemiCircleRotDownView.create(this)
+        LineBreakSlantLeftView.create(this)
     }
 }
