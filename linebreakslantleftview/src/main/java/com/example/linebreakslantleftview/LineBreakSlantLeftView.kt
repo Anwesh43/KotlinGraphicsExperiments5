2cef14a498ba52dbfd06e8a69164f9937d9d6f24
Anwesh43 <anweshthecool0@gmail.com>
2025-04-25 10:23:38 +0530

    linebreakslantleftview: adding LBSLNode class

--- linebreakslantleftview/src/main/java/com/example/linebreakslantleftview/LineBreakSlantLeftView.kt
+++ linebreakslantleftview/src/main/java/com/example/linebreakslantleftview/LineBreakSlantLeftView.kt
@@ -121,4 +121,45 @@ class LineBreakSlantLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBSLNode? = null
+        private var prev : LBSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSLNode {
+            var curr : LBSLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
