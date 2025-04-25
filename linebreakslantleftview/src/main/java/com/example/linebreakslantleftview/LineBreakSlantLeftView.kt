0dee45c03885a2a16b9859ec0c67351dd9946456
Anwesh43 <anweshthecool0@gmail.com>
2025-04-25 09:29:03 +0530

    linebreakslantleftview: adding LineBreakSlantLeftView class

--- linebreakslantleftview/src/main/java/com/example/linebreakslantleftview/LineBreakSlantLeftView.kt
+++ linebreakslantleftview/src/main/java/com/example/linebreakslantleftview/LineBreakSlantLeftView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLBSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBreakSlantLeft(scale, w, h, paint)
 }
+
+class LineBreakSlantLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
