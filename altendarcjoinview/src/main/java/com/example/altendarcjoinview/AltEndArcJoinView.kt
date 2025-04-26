812b86a7cba123e74bcb86eb0cbe5a5d8dc1da9d
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 14:34:37 +0530

    altendarcjoinview: adding AltEndArcJoin class

--- altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
+++ altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
@@ -164,4 +164,27 @@ class AltEndArcJoinView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AltEndArcJoin(var i : Int) {
+
+        private var curr : AEAJNode = AEAJNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir, {
+                    dir *= -1
+                })
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
