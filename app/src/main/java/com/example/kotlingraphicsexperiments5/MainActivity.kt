7a2279f71cdf0b7d66d518957054f33e1d9e9552
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 15:01:06 +0530

    altendarcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.altendarcjoinview.AltEndArcJoinView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.linebreakslantleftview.LineBreakSlantLeftView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineBreakSlantLeftView.create(this)
+        AltEndArcJoinView.create(this)
     }
 }
