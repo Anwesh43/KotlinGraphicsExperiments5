c1b11f952ec0198354e4d77394dad871786d2e4e
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 14:10:31 +0530

    altendarcjoinview: adding Canvas extension methods

--- altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
+++ altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
@@ -28,3 +28,35 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawAltEndArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * dsc(1), h /2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -rot, deg * (dsc(0) + dsc(2)), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawAEAJNode(i  : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAltEndArcJoin(scale, w, h, paint)
+}
