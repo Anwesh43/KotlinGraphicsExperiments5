48c2ff76b46d92c579723bd177e08c2f6e866f4a
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 14:12:01 +0530

    altendarcjoinview: adding AltEndArcJoinView class

--- altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
+++ altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawAEAJNode(i  : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawAltEndArcJoin(scale, w, h, paint)
 }
+
+class AltEndArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
