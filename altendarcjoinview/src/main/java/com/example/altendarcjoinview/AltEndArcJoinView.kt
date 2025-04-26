4316a28d4e634bbc1fa658c731d0f81771fcc5d8
Anwesh43 <anweshthecool0@gmail.com>
2025-04-26 14:31:45 +0530

    altendarcjoinview: adding AEAJNode class

--- altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
+++ altendarcjoinview/src/main/java/com/example/altendarcjoinview/AltEndArcJoinView.kt
@@ -123,4 +123,45 @@ class AltEndArcJoinView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AEAJNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : AEAJNode? = null
+        private var prev : AEAJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AEAJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAEAJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AEAJNode {
+            var curr : AEAJNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
