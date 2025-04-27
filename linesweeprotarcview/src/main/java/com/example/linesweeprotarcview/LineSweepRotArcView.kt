bda82de7bb0f1003cc9ac9e926ff4aa116f52939
Anwesh43 <anweshthecool0@gmail.com>
2025-04-27 10:41:08 +0530

    linesweeprotarcview: adding Canvas extension methods

--- linesweeprotarcview/src/main/java/com/example/linesweeprotarcview/LineSweepRotArcView.kt
+++ linesweeprotarcview/src/main/java/com/example/linesweeprotarcview/LineSweepRotArcView.kt
@@ -27,3 +27,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineSweepRotArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * (1 + dsc(4)), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(j + 1) * (1 + j) * (1f - 2 * j))
+                drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -rot, rot * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawLSRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineSweepRotArc(scale, w, h, paint)
+}
