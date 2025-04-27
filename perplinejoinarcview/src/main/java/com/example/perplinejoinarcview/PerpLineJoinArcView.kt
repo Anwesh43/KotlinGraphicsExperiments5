1f03b87a43279620ef04a6441f75f5f5d37ca0de
Anwesh43 <anweshthecool0@gmail.com>
2025-04-28 02:14:25 +0530

    perplinejoinarcview: adding PerpLineJoinArc class

--- perplinejoinarcview/src/main/java/com/example/perplinejoinarcview/PerpLineJoinArcView.kt
+++ perplinejoinarcview/src/main/java/com/example/perplinejoinarcview/PerpLineJoinArcView.kt
@@ -161,4 +161,27 @@ class PerpLineJoinArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PerpLineJoinArc(var i : Int = 0, val state : State = State()) {
+
+        private var curr : PLJANode = PLJANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
