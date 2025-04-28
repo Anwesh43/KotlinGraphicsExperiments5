2ae27e3453eac36da10a0eae48a131aa67cb520b
Anwesh43 <anweshthecool0@gmail.com>
2025-04-29 01:28:06 +0530

    perplinearcrotview: adding Renderer class

--- perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
+++ perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
@@ -188,4 +188,27 @@ class PerpLineArcRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : PerpLineArcRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val plar : PerpLineArcRot = PerpLineArcRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            plar.draw(canvas, paint)
+            animator.animate {
+                plar.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            plar.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
