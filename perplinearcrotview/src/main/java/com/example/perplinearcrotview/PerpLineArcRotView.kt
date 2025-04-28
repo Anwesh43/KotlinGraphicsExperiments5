b726a09673acb21409febbb11fc6d5975d5294c2
Anwesh43 <anweshthecool0@gmail.com>
2025-04-29 01:13:44 +0530

    perplinearcrotview: adding Canvas extension methods

--- perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
+++ perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
@@ -22,8 +22,43 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 90f
+val rot : Float = 180f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawPerpLineArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawArc(RectF(0f, -size / 2, size, 0f), 180f, rot * dsc(0), false, paint)
+        }
+        drawXY(-size, -h * 0.5f * (1 - dsc(2))) {
+            rotate(deg * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawPLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawPerpLineArcRot(scale, w, h, paint)
+}
