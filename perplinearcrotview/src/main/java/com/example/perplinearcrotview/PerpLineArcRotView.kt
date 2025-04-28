15ab9102f8d902f4b0a6e709264101abb397ecdf
Anwesh43 <anweshthecool0@gmail.com>
2025-04-29 01:24:32 +0530

    perplinearcrotview: adding PerpLineArcRot class

--- perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
+++ perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
@@ -165,4 +165,27 @@ class PerpLineArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PerpLineArcRot(var i : Int) {
+
+        private var curr : PLARNode = PLARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
