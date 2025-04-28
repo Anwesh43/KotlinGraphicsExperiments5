8c7b852db451af15d5a74c8556a5e61e18df9432
Anwesh43 <anweshthecool0@gmail.com>
2025-04-29 01:21:10 +0530

    perplinearcrotview: adding Animator class

--- perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
+++ perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
@@ -97,4 +97,31 @@ class PerpLineArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
