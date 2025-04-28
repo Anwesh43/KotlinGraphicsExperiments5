6efdc1dfe920c0383fe24f1ae295a438cc2ad4ea
Anwesh43 <anweshthecool0@gmail.com>
2025-04-29 01:23:15 +0530

    perplinearcrotview: adding PLARNode class

--- perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
+++ perplinearcrotview/src/main/java/com/example/perplinearcrotview/PerpLineArcRotView.kt
@@ -124,4 +124,45 @@ class PerpLineArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class PLARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : PLARNode? = null
+        private var prev : PLARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = PLARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawPLARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : PLARNode {
+            var curr : PLARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
