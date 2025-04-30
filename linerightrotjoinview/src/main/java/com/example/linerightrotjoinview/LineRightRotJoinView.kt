74f04731242c5c5c0db0b60f49d76c792f5a2ecd
Anwesh43 <anweshthecool0@gmail.com>
2025-05-01 02:02:02 +0530

    linerightrotjoinview: adding LineRightRotJoinView class

--- linerightrotjoinview/src/main/java/com/example/linerightrotjoinview/LineRightRotJoinView.kt
+++ linerightrotjoinview/src/main/java/com/example/linerightrotjoinview/LineRightRotJoinView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLRRJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRightRotJoin(scale, w, h, paint)
 }
+
+class LineRightRotJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
