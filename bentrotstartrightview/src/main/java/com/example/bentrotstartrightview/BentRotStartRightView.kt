e1fa95213babb62ae939c7e0a6c65a1a95dcb971
Anwesh43 <anweshthecool0@gmail.com>
2025-05-02 01:40:48 +0530

    bentrotstartrightview: adding BRSRNode class

--- bentrotstartrightview/src/main/java/com/example/bentrotstartrightview/BentRotStartRightView.kt
+++ bentrotstartrightview/src/main/java/com/example/bentrotstartrightview/BentRotStartRightView.kt
@@ -119,4 +119,45 @@ class BentRotStartRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BRSRNode? = null
+        private var prev : BRSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRSRNode {
+            var curr : BRSRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
