5e806c86a841344a36808b8abf276af9c3db8991
Anwesh43 <anweshthecool0@gmail.com>
2025-05-03 11:30:01 +0530

    multilinerotfoldview: adding Canvas extension methods

--- multilinerotfoldview/src/main/java/com/example/multilinerotfoldview/MultiFoldRotLineView.kt
+++ multilinerotfoldview/src/main/java/com/example/multilinerotfoldview/MultiFoldRotLineView.kt
@@ -28,3 +28,36 @@ val lines : Int = 5
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMultiLineRotFold(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val linesMid = lines / 2
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        for (j in 0..(lines - 1)) {
+            val dsk : Float = dsc(0).divideScale(j, lines)
+            drawXY(0f, 0f) {
+                rotate(-90 + rot * j * (1f - dsc(Math.abs(linesMid - j))))
+                drawLine(0f, 0f, size * dsk, 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawMLRFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMultiLineRotFold(scale, w, h, paint)
+}
