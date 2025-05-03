4df89c03858373ef86d5b3d098e8d26446174576
Anwesh43 <anweshthecool0@gmail.com>
2025-05-04 01:07:56 +0530

    eithersidearccompleteview: adding Canvas extension methods

--- eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
+++ eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
@@ -19,7 +19,7 @@ val colors : Array<String> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFacro : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawEitherSideArcComplete(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * dsc(1))
+                drawArc(RectF(-size, -size / 2, 0f, size / 2), 0f, 180f * dsc(0), false, paint)
+            }
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(2), paint)
+    }
+}
+
+fun Canvas.drawESACNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawEitherSideArcComplete(scale, w, h, paint)
+}
