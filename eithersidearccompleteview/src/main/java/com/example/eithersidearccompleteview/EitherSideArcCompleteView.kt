a8e65cf0a869b3fb0ff1cae3eca71ac5db82d832
Anwesh43 <anweshthecool0@gmail.com>
2025-05-04 01:09:44 +0530

    eithersidearccompleteview: adding EitherSideArcCompleteView class

--- eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
+++ eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawESACNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawEitherSideArcComplete(scale, w, h, paint)
 }
+
+class EitherSideArcCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
