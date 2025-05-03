fb41a2537c030aed2151572cf4c58beb2f249340
Anwesh43 <anweshthecool0@gmail.com>
2025-05-04 01:38:54 +0530

    eithersidearccompleteview: adding ESACNode class

--- eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
+++ eithersidearccompleteview/src/main/java/com/example/eithersidearccompleteview/EitherSideArcCompleteView.kt
@@ -124,4 +124,45 @@ class EitherSideArcCompleteView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ESACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ESACNode? = null
+        private var prev : ESACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ESACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawESACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ESACNode {
+            var curr : ESACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
