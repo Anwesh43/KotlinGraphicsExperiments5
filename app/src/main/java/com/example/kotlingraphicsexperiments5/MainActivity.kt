62434e4dd1360fd84b7fce306a3981bfa3f0266d
Anwesh43 <anweshthecool0@gmail.com>
2025-05-04 01:50:53 +0530

    eithersidearccompleteview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsexperiments5/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.eithersidearccompleteview.EitherSideArcCompleteView
 import com.example.kotlingraphicsexperiments5.ui.theme.KotlinGraphicsExperiments5Theme
-import com.example.multilinerotfoldview.MultiFoldRotLineView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MultiFoldRotLineView.create(this)
+        EitherSideArcCompleteView.create(this)
     }
 }
