c83b7222fdf83a4e318343047e1f6eb09cb1d954
Anwesh43 <anweshthecool0@gmail.com>
2025-05-05 08:46:15 +0530

    quarterarcsemidownview: adding QuarterSemiCircleDownView class

--- quarterarcsemidownview/src/main/java/com/example/quarterarcsemidownview/QuarterSemiCircleDownView.kt
+++ quarterarcsemidownview/src/main/java/com/example/quarterarcsemidownview/QuarterSemiCircleDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawQASDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawQuarterArcSemiDown(scale, w, h, paint)
 }
+
+class QuarterSemiCircleDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
