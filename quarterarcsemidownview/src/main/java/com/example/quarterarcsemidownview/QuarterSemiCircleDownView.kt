a819eb358f08b0ecad227d0547dda0bfccae196a
Anwesh43 <anweshthecool0@gmail.com>
2025-05-05 08:41:37 +0530

    quarterarcsemidownview: adding Canvas extension methods

--- quarterarcsemidownview/src/main/java/com/example/quarterarcsemidownview/QuarterSemiCircleDownView.kt
+++ quarterarcsemidownview/src/main/java/com/example/quarterarcsemidownview/QuarterSemiCircleDownView.kt
@@ -28,3 +28,37 @@ val sweep : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawQuarterArcSemiDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(-rot * dsc(2))
+                drawXY(-size, 0f) {
+                    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -sweep, sweep * dsc(j), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawQASDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawQuarterArcSemiDown(scale, w, h, paint)
+}
